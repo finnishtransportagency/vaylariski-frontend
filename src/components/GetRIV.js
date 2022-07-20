d4e45f2eecebda52411d3543597130a87e67544e
Katri Seto <katri.seto@gmail.com>
2022-07-20 13:41:03 +0300

    GetRIV updated

--- src/components/GetRIV.js
+++ src/components/GetRIV.js
@@ -4,8 +4,8 @@ import React from "react";
 import axios from "axios";
 
 const client = axios.create({
-    baseURL: "https://jsonplaceholder.typicode.com/posts" 
-    //baseURL: "http://127.0.0.1:8000/fairway/helsinki"
+    //baseURL: "https://jsonplaceholder.typicode.com/posts" 
+    baseURL: "http://127.0.0.1:8000/fairway/helsinki"
   
   });
 
@@ -21,18 +21,21 @@ const client = axios.create({
     React.useEffect(() => {
         const fetchPost = async () => {
            let response = await client.get();
-           setPosts(response.data);
+           setPosts(response.data.helsinki_west.sites);
+           console.log(response.data)
         };
         fetchPost();
     }, []);   
   
-
+ 
     return (
         <ul className="posts">
           {posts.map((post) => (
             <li className="post" key={post.id}>
-              <h4>{post.title}</h4>
-              <p>{post.body}</p>
+              <h4>{post.id}</h4>
+              <p>Number of lanes: {post.number_of_lanes}</p>
+              <p>Aids to navigation: {post.aids_to_navigation}</p>
+              <p>Bottom surface: {post.bottom_surface}</p>
             </li>
           ))}
         </ul>
