7594375b44de71a28642c217cd0d6f881bbd80f3
Katri Seto <katri.seto@gmail.com>
2022-07-20 16:16:42 +0300

    button added

--- src/components/GetRIV.js
+++ src/components/GetRIV.js
@@ -3,6 +3,45 @@
 import React from "react";
 import axios from "axios";
 
+import styled from 'styled-components';
+
+const Button = styled.button`
+   background-color: #ffe082;
+   color: white; 
+   padding: 5px 15px;
+   border-radius: 5px;
+   outline: 0;
+   text-transform: uppercase;
+   cursor: pointer;
+   box-shadow: 0px 2px 2px lightgray;
+   transition: ease background-color 250ms;
+   & hover {
+    background-color: white;
+   }
+   `  
+
+function clickMe(){
+  alert('You clicked me!');
+}
+
+function ButtonClick() {
+  return(
+    <>
+    <div>
+      <Button onClick={clickMe}>
+        Button 
+      </Button>
+     </div> 
+
+     <div>
+     <Button onClick={clickMe}>
+       Button 
+     </Button>
+    </div> 
+    </>
+  );
+}
+
 const client = axios.create({
     //baseURL: "https://jsonplaceholder.typicode.com/posts" 
     baseURL: "http://127.0.0.1:8000/fairway/helsinki"
@@ -30,6 +69,7 @@ const client = axios.create({
  
     return (
         <ul className="posts">
+        <ButtonClick/>
           {posts.map((post) => (
             <li className="post" key={post.id}>
               <h4>{post.id}</h4>
