c22c5b4f7403d3a01a79e7fc30e67be9bd4b4339
Juha Keponen <juha.keponen@cgi.com>
2022-08-15 13:27:38 +0300

    Display RIV results component

--- src/components/CalculateRIV.js
+++ src/components/CalculateRIV.js
@@ -1,13 +1,18 @@
 import {useForm} from "react-hook-form";
 import axios from "axios";
 import { getDefaultNormalizer } from "@testing-library/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import DisplayRIVResults from "./DisplayRIVResults";
 
 
 function CalculateRIV() {
-  
+  //Kysyy input arvot käyttäjältä
+  const {register,handleSubmit,formState:{errors}} =useForm();
+  const onSubmit = (data) => fetchRiskValue(data); //Input arvot laskennalle
+  const [RIVResults, setRIVResults] = useState([]);
+
   const api = axios.create({
-    baseURL: "http://127.0.0.1:8000/" 
+    baseURL: "http://127.0.0.1:8000/"
   });
 
   //Kutsuu calculate_risk endpointtia parametreillä
@@ -16,30 +21,32 @@ function CalculateRIV() {
     console.log('You clicked me!' + JSON.stringify(data));
     const response = await api.post(path, data);
     console.log(response.data);
-
+    setRIVResults(response.data.RIV)
   }
-  
-  //Kysyy input arvot käyttäjältä
-  const {register,handleSubmit,formState:{errors}} =useForm();
-  const onSubmit = (data) => fetchRiskValue(data); //Input arvot laskennalle
+
+  useEffect(() => {
+    console.log(RIVResults);
+  }, [RIVResults]);
+
 
   return (
+    <>
     <form onSubmit={handleSubmit(onSubmit)}>
       <p>Enter fairway</p>
       <div>
-        <label>Fairway: </label>       
+        <label>Fairway: </label>
         <select{...register("fairway")}>
           <option defaultValue="Helsinki">Helsinki</option>
           <option value="Oulu">Oulu</option>
           <option value="Rauma">Rauma</option>
           <option value="Saimaa">Saimaa</option>
           <option value="Turku">Turku</option>
-        </select>     
+        </select>
       </div>
       <p>Enter boat parameters</p>
       <div>
         <label>
-        Speed (knots): 
+        Speed (knots):
           <input {...register("boat.speed", {valueAsNumber: true})}
           type="number"
           required
@@ -49,7 +56,7 @@ function CalculateRIV() {
       <div>
         <label>
         Beam (m):
-          <input {...register("boat.beam", {valueAsNumber: true})} 
+          <input {...register("boat.beam", {valueAsNumber: true})}
           type="number"
           required
           />
@@ -58,7 +65,7 @@ function CalculateRIV() {
       <div>
         <label>
         Draft (m):
-          <input {...register("boat.draft", {valueAsNumber: true})} 
+          <input {...register("boat.draft", {valueAsNumber: true})}
           type="number"
           required
           />
@@ -67,7 +74,7 @@ function CalculateRIV() {
       <div>
         <label>
           Length (m):
-          <input {...register("boat.length", {valueAsNumber: true})} 
+          <input {...register("boat.length", {valueAsNumber: true})}
           type="number"
           required
           />
@@ -75,7 +82,7 @@ function CalculateRIV() {
       </div>
 
       <div>
-      <label>Manoeuvrability: </label> 
+      <label>Manoeuvrability: </label>
       <select{...register("boat.manoeuvrability")}>
           <option defaultValue="good">good</option>
           <option value="moderate">moderate</option>
@@ -83,15 +90,17 @@ function CalculateRIV() {
       </select>
       </div>
 
-      <button 
+    <button
       type="submit"
       onClick={handleSubmit}
       >Submit
       </button>
     </form>
-    
-  );
-}
+
+    <DisplayRIVResults rows={RIVResults}  />
+    </>
+    );
+  }
 
 export default CalculateRIV;
 
---
+++ src/components/DisplayRIVResults.js
@@ -0,0 +1,76 @@
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+
+const columns = [
+  { field: 'Site', headerName: 'Site', width: 90 },
+  {
+    field: 'RIV_1',
+    headerName: 'RIV_1',
+    type: 'number',
+    width: 90,
+    editable: false,
+  },
+  {
+    field: 'RIV_2',
+    headerName: 'RIV_2',
+    type: 'number',
+    width: 90,
+    editable: false,
+  },
+  {
+    field: 'RIV_3',
+    headerName: 'RIV_3',
+    type: 'number',
+    width: 90,
+    editable: false,
+  },
+  {
+    field: 'RIV_4',
+    headerName: 'RIV_4',
+    type: 'number',
+    width: 90,
+    editable: false,
+  },
+  {
+    field: 'RIV_5',
+    headerName: 'RIV_5',
+    type: 'number',
+    width: 90,
+    editable: false,
+  },
+  {
+    field: 'RIV_6',
+    headerName: 'RIV_6',
+    type: 'number',
+    width: 90,
+    editable: false,
+  },
+  {
+    field: 'RIV_SUM',
+    headerName: 'RIV_SUM',
+    type: 'number',
+    width: 90,
+    editable: false,
+  },
+];
+
+
+function DisplayRIVResults(props) {
+  return (
+    <Box sx={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={props.rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        disableSelectionOnClick
+        experimentalFeatures={{ newEditingApi: true }}
+        getRowId={(row) => row.Site}
+      />
+    </Box>
+  );
+}
+
+export default DisplayRIVResults;
