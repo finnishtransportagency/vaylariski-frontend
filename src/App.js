e86b2573f5713e2df79ac8b81bd790d8716185ed
Juha Keponen <juha.keponen@cgi.com>
2022-11-17 16:35:12 +0200

    removed map

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import CalculateRIV from "./components/CalculateRIV";
 import "./App.css";
-import Mapp from "./components/Mapp";
+// import Mapp from "./components/Mapp";
 import TestButton from "./components/TestButton";
 
 function App() {
