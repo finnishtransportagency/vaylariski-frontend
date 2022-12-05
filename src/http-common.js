bdbb32070ae39fc15e99b2f62100c98916e769ce
Juha Keponen <juha.keponen@cgi.com>
2022-12-05 10:26:59 +0200

    aws dockerfile configs, buildspec secret

--- src/http-common.js
+++ src/http-common.js
@@ -1,11 +1,6 @@
 import axios from "axios";
 
-// const BaseRestURL =
-
-
-// console.log(process.env.REACT_APP_BASE_REST_URL)
-// console.log(BaseRestURL)
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_BASE_REST_URL || "api"
+  baseURL: process.env.REACT_APP_BASE_REST_URL || "vaylariski/rest"
 });
