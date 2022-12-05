fe36956f99def83612ddce3176938841dd3f8e89
Juha Keponen <juha.keponen@cgi.com>
2022-12-05 16:17:14 +0200

    attempt clear previous markers

--- src/components/Mapp.js
+++ src/components/Mapp.js
@@ -21,47 +21,128 @@ const geojsonMarkerOptionsRed = {
   fillOpacity: 0.8
 };
 
-function Mapp() {
-  const mapRef = useRef();
-  // console.log(mapRef);
+function GeoJSONMarkers() {
+  const map = useMap();
   const { RIVResults, setRIVResults } = useContext(RIVResultContext);
+  let geoJSONLayers = new L.LayerGroup();
 
-  function onEachFeature(feature, layer) {
-    // does this feature have a property named popupContent?
-    if (feature.properties) {
-      // layer.bindPopup(`<p>${feature.properties.GDO_GID}</p>`)
-      layer.bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
-    }
-  };
-
+  console.log('lapsi', map);
   useEffect(() => {
-    const { current = {} } = mapRef;
-    if (!current) return;
+    map.removeLayer(geoJSONLayers);
+    geoJSONLayers.clearLayers();
+    console.log(geoJSONLayers);
 
-    const map = mapRef.current;
-    if ( !map ) return;
+    function onEachFeature(feature, layer) {
+      // does this feature have a property named popupContent?
+      if (feature.properties) {
+        // layer.bindPopup(`<p>${feature.properties.GDO_GID}</p>`)
+        layer.bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
+      }
+    };
 
-    L.geoJSON(RIVResults, {
+    const layers = new L.GeoJSON(RIVResults, {
       onEachFeature: onEachFeature,
       pointToLayer: function (feature, latlng) {
+        // Initial traffic lights for risk value
         if (feature.properties.RISK_INDEX_SUM > 20 ) {
           return L.circleMarker(latlng, geojsonMarkerOptionsRed);
         }
-          return L.circleMarker(latlng, geojsonMarkerOptions);
-        }
-   }).addTo(map);
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+      }
+    });
+    layers.addTo(geoJSONLayers);
+
+    console.log('geoJSONLayers', geoJSONLayers);
+    geoJSONLayers.addTo(map);
+
 
   }, [RIVResults]);
+  return null;
+}
+
+function Mapp() {
+  // Map ref
+  const mapRef = useRef(null);
+  // Map state
+  const [mapInstance, setMapInstance] = useState(null);
+
+  const { RIVResults, setRIVResults } = useContext(RIVResultContext);
+  let markersLayerGroup = new L.LayerGroup();
+  // const [geoJSONLayers, setGeoJSONLayes]  = useState();
+
+  // useEffect(() => {
+  //   // let map = mapRef.current;
+  //   // if (!mapRef.current) return;
+  //   // Set map instance to state:
+  //   console.log('[]',mapRef.current);
+  //   setMapInstance(mapRef.current);
+  // }, []);
+
+  // useEffect(() => {
+
+  //   console.log('mapinstance', mapInstance);
+  // }, [mapInstance]);
+
+  // // useEffect(() => {
+  // //   // Check for the map instance before adding something (ie: another event listener).
+  // //   // If no map, return:
+  // //   if (!mapInstance) return;
+
+  // // }, [mapInstance]);
+
+  // function onEachFeature(feature, layer) {
+  //   // does this feature have a property named popupContent?
+  //   if (feature.properties) {
+  //     // layer.bindPopup(`<p>${feature.properties.GDO_GID}</p>`)
+  //     layer.bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   // geoJSONLayers.clearLayers();
+  //   // console.log('markersLayerGroup layer len',geoJSONLayers);
+
+  //   // const { current = {} } = mapRef;
+  //   // if (!current) return;
+
+  //   // const map = mapRef.current;
+  //   // if ( !map ) return;
+  //   // geoJSONLayers.removeFrom(map);
+
+  //   const geoJSONLayers = new L.GeoJSON(RIVResults, {
+  //     onEachFeature: onEachFeature,
+  //     pointToLayer: function (feature, latlng) {
+  //       // Initial traffic lights for risk value
+  //       if (feature.properties.RISK_INDEX_SUM > 20 ) {
+  //         return L.circleMarker(latlng, geojsonMarkerOptionsRed);
+  //       }
+  //       return L.circleMarker(latlng, geojsonMarkerOptions);
+  //     }
+  //   });//.addTo(markersLayerGroup);
+  //   console.log('geoJSONLayers', geoJSONLayers);
+  //   // lolgeo.addTo(markersLayerGroup);
+
+  //   console.log('markersLayerGroup after',markersLayerGroup.getLayers().length);
+  //   if (mapRef.current) {
+  //     console.log('should add to mapinstance');
+  //     geoJSONLayers.addTo(mapRef.current);
+  //   }
+
+  // }, [RIVResults]);
+
 
   return (
     <MapContainer
-      ref={mapRef}
+      // whenReady={ instance => {mapRef.current = instance} }
+      // ref={mapRef}
       center={[62, 23.5]}
       zoom={5}
       scrollWheelZoom={false}
-      style={{ height:"800px",backgroundColor:"red",marginTop:"80px", marginBottom:'90px'
+      style={{ height:"800px",backgroundColor:"white",marginTop:"80px", marginBottom:'90px'
         }}
     >
+      <GeoJSONMarkers />
+
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
