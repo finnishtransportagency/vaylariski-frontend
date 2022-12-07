48c52c87400f632d1e0705c46fa4dd3c7eef849d
Juha Keponen <juha.keponen@cgi.com>
2022-12-07 16:14:21 +0200

    pretty lines

--- src/components/BoatMenuComponent.js
+++ src/components/BoatMenuComponent.js
@@ -73,7 +73,7 @@ export default function BoatMenuComponent(props) {
                 Manoeuvrability: {boat.Manoeuvrability}
             </MenuItem>
           ))}
-        </Menu>
-      </>
-    );
-  };
+      </Menu>
+    </>
+  );
+};
