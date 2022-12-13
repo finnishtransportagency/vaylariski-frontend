763ec4c84234bb8ad59dc90eefc77f642a726db7
Juha Keponen <juha.keponen@cgi.com>
2022-12-13 15:30:53 +0200

    Removed testing button

--- src/components/NotificationComponent.js
+++ src/components/NotificationComponent.js
@@ -1,15 +1,11 @@
 import { Button, Snackbar } from "@mui/material";
 import MuiAlert from "@mui/material/Alert";
 import NotificationContext from "contexts/NotificationContext";
-import { forwardRef, useContext, useState } from "react";
+import { useContext } from "react";
 
 
 export default function NotificationComponent() {
   const {notificationStatus, setNotificationStatus} = useContext(NotificationContext);
-
-  const handleClick = () => {
-    setNotificationStatus({...notificationStatus, visible: true});
-  };
 
   const handleClose = (event, reason) => {
     if (reason === "clickaway") {
@@ -20,9 +16,6 @@ export default function NotificationComponent() {
 
   return (
     <>
-      <Button variant="outlined" onClick={handleClick}>
-        Open success snackbar
-      </Button>
       <Snackbar open={notificationStatus.visible} autoHideDuration={6000} onClose={handleClose}>
         <MuiAlert
           elevation={6}
