ce4cd82b7183c51b353acf2b66385af8b6fe7285
Juha Keponen <juha.keponen@cgi.com>
2022-12-21 09:47:21 +0200

    Traffic lights position

--- src/components/MapComponent.js/MapComponent.js
+++ src/components/MapComponent.js/MapComponent.js
@@ -1,10 +1,10 @@
-import { useContext, useEffect, useState } from 'react';
-import L, { FeatureGroup } from 'leaflet'
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import { useContext, useEffect, useState } from "react";
+import L, { FeatureGroup } from "leaflet";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 
-import RIVResultContext from '../../contexts/RIVResult';
-import RIVTrafficLightContext from '../../contexts/RIVTrafficLightContext';
-import RIVTrafficLightsComponent from './RIVTrafficLightsComponent';
+import RIVResultContext from "../../contexts/RIVResult";
+import RIVTrafficLightContext from "../../contexts/RIVTrafficLightContext";
+import RIVTrafficLightsComponent from "./RIVTrafficLightsComponent";
 
 const geojsonMarkerOptionsGreen = {
   radius: 8,
@@ -12,7 +12,7 @@ const geojsonMarkerOptionsGreen = {
   color: "#00FF00",
   weight: 1,
   opacity: 1,
-  fillOpacity: 0.8
+  fillOpacity: 0.8,
 };
 const geojsonMarkerOptionsYellow = {
   radius: 8,
@@ -20,7 +20,7 @@ const geojsonMarkerOptionsYellow = {
   color: "#ffff00",
   weight: 1,
   opacity: 1,
-  fillOpacity: 0.8
+  fillOpacity: 0.8,
 };
 const geojsonMarkerOptionsRed = {
   radius: 8,
@@ -28,21 +28,32 @@ const geojsonMarkerOptionsRed = {
   color: "#ff0000",
   weight: 1,
   opacity: 1,
-  fillOpacity: 0.8
+  fillOpacity: 0.8,
 };
 
 function GeoJSONMarkers() {
   const map = useMap();
   const { RIVResults, setRIVResults } = useContext(RIVResultContext);
-  const { RIVTrafficLight, setRIVTraffiLight } = useContext(RIVTrafficLightContext);
-  const [geojsonFeatGroup, setGeojsonFeatGroup ] = useState(new L.FeatureGroup());
+  const { RIVTrafficLight, setRIVTraffiLight } = useContext(
+    RIVTrafficLightContext
+  );
+  const [geojsonFeatGroup, setGeojsonFeatGroup] = useState(
+    new L.FeatureGroup()
+  );
 
   function onEachFeature(feature, layer) {
     // If feature have have properties parse all of them and bind to layer
     if (feature.properties) {
-      layer.bindPopup('<pre>'+JSON.stringify(feature.properties,null,' ').replace(/[\{\}"]/g,'')+'</pre>');
+      layer.bindPopup(
+        "<pre>" +
+          JSON.stringify(feature.properties, null, " ").replace(
+            /[\{\}"]/g,
+            ""
+          ) +
+          "</pre>"
+      );
     }
-  };
+  }
 
   useEffect(() => {
     setGeojsonFeatGroup(geojsonFeatGroup.clearLayers());
@@ -51,14 +62,16 @@ function GeoJSONMarkers() {
       onEachFeature: onEachFeature,
       pointToLayer: function (feature, latlng) {
         // Initial traffic lights for risk value
-        if (feature.properties.RISK_INDEX_SUM < RIVTrafficLight.green ) {
+        if (feature.properties.RISK_INDEX_SUM < RIVTrafficLight.green) {
           return L.circleMarker(latlng, geojsonMarkerOptionsGreen);
-        } else if (feature.properties.RISK_INDEX_SUM >= RIVTrafficLight.green &&
-          feature.properties.RISK_INDEX_SUM < RIVTrafficLight.yellow ) {
+        } else if (
+          feature.properties.RISK_INDEX_SUM >= RIVTrafficLight.green &&
+          feature.properties.RISK_INDEX_SUM < RIVTrafficLight.yellow
+        ) {
           return L.circleMarker(latlng, geojsonMarkerOptionsYellow);
         }
         return L.circleMarker(latlng, geojsonMarkerOptionsRed);
-      }
+      },
     });
     setGeojsonFeatGroup(layers.addTo(geojsonFeatGroup));
 
@@ -70,26 +83,28 @@ function GeoJSONMarkers() {
 function MapComponent() {
   return (
     <>
-
-    <MapContainer
-      // whenReady={ instance => {mapRef.current = instance} }
-      // ref={mapRef}
-      center={[62, 23.5]}
-      zoom={5}
-      scrollWheelZoom={false}
-      style={{ height:"800px",backgroundColor:"white",marginTop:"80px", marginBottom:'5px'
+      <RIVTrafficLightsComponent />
+      <MapContainer
+        // whenReady={ instance => {mapRef.current = instance} }
+        // ref={mapRef}
+        center={[62, 23.5]}
+        zoom={5}
+        scrollWheelZoom={false}
+        style={{
+          height: "800px",
+          backgroundColor: "white",
+          marginTop: "80px",
+          marginBottom: "5px",
         }}
-    >
-      <GeoJSONMarkers
-      />
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
-    <RIVTrafficLightsComponent />
+      >
+        <GeoJSONMarkers />
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
     </>
-    );
-};
+  );
+}
 
 export default MapComponent;
