29a8562332ce3b982b7c1c1e691a3dcbf2401740
Juha Keponen <juha.keponen@cgi.com>
2023-01-17 11:57:21 +0200

    fix

---
+++ src/views/InsertNewBulkBoatComponent.js
@@ -0,0 +1,106 @@
+import { CustomNumber } from "components/CustomNumber";
+import { CustomText } from "components/CustomText";
+import { Field } from "formik";
+import { Formik } from "formik";
+import { Form as FForm } from "formik";
+import { Button } from "react-bootstrap";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  PITUUS: Yup.number()
+    .min(0, "Pituus ei voi olla negatiivinen")
+    .typeError("Pituus tulee olla numero")
+    .required("Pituus vaaditaan"),
+  LEVEYS: Yup.number()
+    .min(0, "Leveys ei voi olla negatiivinen")
+    .typeError("Leveys tulee olla numero")
+    .required("Leveys vaaditaan"),
+  SYVAYS: Yup.number()
+    .min(0, "Syväys ei voi olla negatiivinen")
+    .typeError("Syväys tulee olla numero")
+    .required("Syväys vaaditaan"),
+  JNRO: Yup.number()
+    .min(0, "JNRO ei voi olla negatiivinen")
+    .typeError("JNRO tulee olla numero"),
+  KOKO: Yup.number()
+    .min(0, "Koko ei voi olla negatiivinen")
+    .typeError("Koko tulee olla numero"),
+  RUNKO_TKERROIN: Yup.number()
+    .min(0, "RUNKO_TKERROIN ei voi olla negatiivinen")
+    .typeError("RUNKO_TKERROIN tulee olla numero"),
+  SELITE: Yup.string().trim(),
+  VAY_NIMISU: Yup.string().trim(),
+});
+
+export default function InsertNewBulkBoatComponent(props) {
+  const { children, tabValue, tabIndex, ...other } = props;
+  return (
+    <div
+      role="TabPanelComponent"
+      hidden={tabValue !== tabIndex}
+      id={`simple-tabpanel-${tabIndex}`}
+      aria-labelledby={`simple-tab-${tabIndex}`}
+      {...other}
+    >
+      {tabValue === tabIndex && (
+        <Formik
+          initialValues={{
+            PITUUS: "",
+            LEVEYS: "",
+            SYVAYS: "",
+            JNRO: "",
+            KOKO: "",
+            RUNKO_TKERROIN: "",
+            SELITE: "",
+            VAY_NIMISU: "",
+          }}
+          onSubmit={(values) => {
+            throw new Error("Function not implemented.");
+          }}
+          validationSchema={validationSchema}
+          enableReinitialize
+          validateOnMount
+        >
+          {(formik) => (
+            <FForm onSubmit={formik.handleSubmit}>
+              <CustomNumber
+                label="Laivan pituus"
+                name="PITUUS"
+                readOnly={false}
+              />
+              <CustomNumber
+                label="Laivan leveys"
+                name="LEVEYS"
+                readOnly={false}
+              />
+              <CustomNumber
+                label="Laivan syväys"
+                name="SYVAYS"
+                readOnly={false}
+              />
+              <CustomNumber label="jnro" name="JNRO" readOnly={false} />
+              <CustomNumber label="Laivan koko" name="KOKO" readOnly={false} />
+              <CustomText
+                label="RUNKO_TKERROIN"
+                name="RUNKO_TKERROIN"
+                readOnly={false}
+              />
+              <CustomText label="Selite" name="SELITE" readOnly={false} />
+              <CustomText
+                label="VAY_NIMISU"
+                name="VAY_NIMISU"
+                readOnly={false}
+              />
+              <Button
+                type="submit"
+                disabled={formik.isSubmitting || !formik.isValid}
+              >
+                Lähetä laivan paramatrit
+              </Button>
+            </FForm>
+          )}
+        </Formik>
+      )}
+    </div>
+  );
+}
