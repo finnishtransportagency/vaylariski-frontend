6d96b0cf39ddc26ce978367a763c627419b63f19
Henriikka Harinen <henriikka.harinen@cgi.com>
2023-03-24 08:06:50 +0200

    Boat not required

--- src/components/UserInputComponent/BoatMenuComponent.js
+++ src/components/UserInputComponent/BoatMenuComponent.js
@@ -74,7 +74,6 @@ export default function BoatMenuComponent(props) {
             <TextField
               style={{ backgroundColor: "white" }}
               {...params}
-              required
             />
           )}
         />
