a2f9e898836c809ac2d96a46370294221fb73f94
Cedric Neergaard <75533906+CeduDev@users.noreply.github.com>
2023-05-30 15:03:16 +0300

    Analpa 2781: Smaller header (#3)
    * added var-colors
    
    * added vayla logo with no text
    
    * separated header into own component
    
    * bootstrap css and logo font-family import
    
    * styling

--- src/index.js
+++ src/index.js
@@ -11,6 +11,16 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+      integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+      crossOrigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Exo+2"
+    />
     <App />
   </React.StrictMode>
 );
