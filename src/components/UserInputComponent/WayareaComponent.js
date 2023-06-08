ffcdc4255135d10c44df0078a4b70a88993a6392
CeduDev <cedric.neergaard@gmail.com>
2023-06-08 16:31:02 +0300

    removed comments

--- src/components/UserInputComponent/WayareaComponent.js
+++ src/components/UserInputComponent/WayareaComponent.js
@@ -76,27 +76,3 @@ export default function WayareaNameComponent(props) {
     </Form.Group>
   );
 }
-/*
-<Field
-  component="select"
-  name="calculation_interval"
-  type="number"
-  required
-  style={{
-    width: 100,
-  }}
->
-  <option value="10">10</option>
-  <option value="20">20</option>
-  <option value="30">30</option>
-  <option value="40">40</option>
-  <option value="50">50</option>
-  <option value="60">60</option>
-  <option value="70">70</option>
-  <option value="80">80</option>
-  <option value="90">90</option>
-  <option value="100">100</option>
-  <option value="1000">1000</option>
-</Field>;
-
-*/
