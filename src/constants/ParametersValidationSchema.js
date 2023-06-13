d93be8cf6da332eaa4807881a95c7fad1dba3c9f
CeduDev <cedric.neergaard@gmail.com>
2023-06-13 10:26:22 +0300

    updated validation error messages

--- src/constants/ParametersValidationSchema.js
+++ src/constants/ParametersValidationSchema.js
@@ -8,122 +8,122 @@ const parametersValidationSchema = Yup.object().shape({
     calculation_params: Yup.object().shape({
       other: Yup.object().shape({
         visibility: Yup.number()
-          .min(0, "visibility ei voi olla negatiivinen")
-          .required("visibility vaaditaan"),
+          .min(0, "Visibility ei voi olla negatiivinen")
+          .required("Visibility vaaditaan"),
       }),
     }),
   }),
 
   boat: Yup.object().shape({
     length: Yup.number()
-      .min(1, "pituus ei voi olla negatiivinen")
-      .required("pituus vaaditaan"),
+      .min(1, "Pituus ei voi olla negatiivinen")
+      .required("Pituus vaaditaan"),
     beam: Yup.number()
-      .min(1, "leveys ei voi olla negatiivinen")
-      .required("leveys vaaditaan"),
+      .min(1, "Leveys ei voi olla negatiivinen")
+      .required("Leveys vaaditaan"),
     draft: Yup.number()
-      .min(1, "syväys ei voi olla negatiivinen")
-      .required("syväys vaaditaan"),
+      .min(1, "Syväys ei voi olla negatiivinen")
+      .required("Syväys vaaditaan"),
   }),
 
   PF_bend_parameters: Yup.object().shape({
     bend_ratio_lim_1: Yup.number()
-      .min(0, "ei voi olla negatiivinen")
+      .min(0, "bend_ration_lim_1 ei voi olla negatiivinen")
       .max(
         Yup.ref("bend_ratio_lim_2"),
-        "ei voi olla isompi kun bend_ratio_lim_2"
+        "bend_ration_lim_1 ei voi olla isompi kun bend_ration_lim_2"
       )
-      .required("vaaditaan"),
+      .required("bend_ration_lim_1 vaaditaan"),
     bend_ratio_lim_2: Yup.number()
       .min(
         Yup.ref("bend_ratio_lim_1"),
-        "ei voi olla pienempi kun bend_ratio_lim_1"
+        "bend_ration_lim_2 ei voi olla pienempi kun bend_ration_lim_1"
       )
       .max(
         Yup.ref("bend_ratio_lim_3"),
-        "ei voi olla isompi kun bend_ratio_lim_3"
+        "bend_ration_lim_2 ei voi olla isompi kun bend_ration_lim_3"
       )
-      .required("vaaditaan"),
+      .required("bend_ration_lim_2 vaaditaan"),
     bend_ratio_lim_3: Yup.number()
       .min(
         Yup.ref("bend_ratio_lim_2"),
-        "ei voi olla pienempi kun bend_ratio_lim_2"
+        "bend_ration_lim_3 ei voi olla pienempi kun bend_ration_lim_2"
       )
       .max(
         Yup.ref("bend_ratio_lim_4"),
-        "ei voi olla isompi kun bend_ratio_lim_4"
+        "bend_ration_lim_3 ei voi olla isompi kun bend_ration_lim_4"
       )
-      .required("vaaditaan"),
+      .required("bend_ration_lim_3 vaaditaan"),
     bend_ratio_lim_4: Yup.number()
       .min(
         Yup.ref("bend_ratio_lim_3"),
-        "ei voi olla pienempi kun bend_ratio_lim_4"
+        "bend_ration_lim_4 ei voi olla pienempi kun bend_ration_lim_3"
       )
-      .required("vaaditaan"),
+      .required("bend_ration_lim_4 vaaditaan"),
     PF_bend_radius_1: Yup.number()
-      .min(0, "ei voi olla negatiivinen")
-      .required("vaaditaan"),
+      .min(0, "PF_bend_radius_1 ei voi olla negatiivinen")
+      .required("PF_bend_radius_1 vaaditaan"),
     PF_bend_radius_2: Yup.number()
-      .min(0, "ei voi olla negatiivinen")
-      .required("vaaditaan"),
+      .min(0, "PF_bend_radius_2 ei voi olla negatiivinen")
+      .required("PF_bend_radius_2 vaaditaan"),
     PF_bend_radius_3: Yup.number()
-      .min(0, "ei voi olla negatiivinen")
-      .required("vaaditaan"),
+      .min(0, "PF_bend_radius_3 ei voi olla negatiivinen")
+      .required("PF_bend_radius_3 vaaditaan"),
     PF_bend_radius_4: Yup.number()
-      .min(0, "ei voi olla negatiivinen")
-      .required("vaaditaan"),
+      .min(0, "PF_bend_radius_4ei voi olla negatiivinen")
+      .required("PF_bend_radius_4 vaaditaan"),
     PF_bend_radius_5: Yup.number()
-      .min(0, "ei voi olla negatiivinen")
-      .required("vaaditaan"),
+      .min(0, "PF_bend_radius_5ei voi olla negatiivinen")
+      .required("PF_bend_radius_5 vaaditaan"),
     bend_angle_lim_1: Yup.number()
-      .min(0, "ei voi negatiivinen")
+      .min(0, "bend_angle_lim_1 ei voi negatiivinen")
       .max(
         Yup.ref("bend_angle_lim_2"),
-        "ei voi olla isompi kun bend_angle_lim_2"
+        "bend_angle_lim_1 ei voi olla isompi kun bend_angle_lim_2"
       )
-      .required("vaaditaan"),
+      .required("bend_angle_lim_1 vaaditaan"),
     bend_angle_lim_2: Yup.number()
       .min(
         Yup.ref("bend_angle_lim_1"),
-        "ei voi olla pienempi kun bend_angle_lim_1"
+        "bend_angle_lim_2 ei voi olla pienempi kun bend_angle_lim_1"
       )
       .max(
         Yup.ref("bend_angle_lim_3"),
-        "ei voi olla isompi kun bend_angle_lim_3"
+        "bend_angle_lim_2 ei voi olla isompi kun bend_angle_lim_3"
       )
-      .required("vaaditaan"),
+      .required("bend_angle_lim_2 vaaditaan"),
     bend_angle_lim_3: Yup.number()
       .min(
-        Yup.ref("bend_angle_lim_3"),
-        "ei voi olla pienempi kun bend_angle_lim_3"
+        Yup.ref("bend_angle_lim_2"),
+        "bend_angle_lim_3 ei voi olla pienempi kun bend_angle_lim_2"
       )
       .max(
         Yup.ref("bend_angle_lim_4"),
-        "ei voi olla isompi kun bend_angle_lim_4"
+        "bend_angle_lim_3 ei voi olla isompi kun bend_angle_lim_4"
       )
-      .required("vaaditaan"),
+      .required("bend_angle_lim_3 vaaditaan"),
     bend_angle_lim_4: Yup.number()
       .min(
         Yup.ref("bend_angle_lim_3"),
-        "ei voi olla pienempi kun bend_angle_lim_3"
+        "bend_angle_lim_4 ei voi olla pienempi kun bend_angle_lim_3"
       )
       .lessThan(360, "pitää olla pienempi kun 360")
-      .required("vaaditaan"),
+      .required("bend_angle_lim_4 vaaditaan"),
     PF_bend_angle_1: Yup.number()
-      .min(0, "ei voi olla negatiivinen")
-      .required("vaaditaan"),
+      .min(0, "PF_bend_angle_1 ei voi olla negatiivinen")
+      .required("PF_bend_angle_1 vaaditaan"),
     PF_bend_angle_2: Yup.number()
-      .min(0, "ei voi olla negatiivinen")
-      .required("vaaditaan"),
+      .min(0, "PF_bend_angle_2 ei voi olla negatiivinen")
+      .required("PF_bend_angle_2 vaaditaan"),
     PF_bend_angle_3: Yup.number()
-      .min(0, "ei voi olla negatiivinen")
-      .required("vaaditaan"),
+      .min(0, "PF_bend_angle_3 ei voi olla negatiivinen")
+      .required("PF_bend_angle_3 vaaditaan"),
     PF_bend_angle_4: Yup.number()
-      .min(0, "ei voi olla negatiivinen")
-      .required("vaaditaan"),
+      .min(0, "PF_bend_angle_4 ei voi olla negatiivinen")
+      .required("PF_bend_angle_4 vaaditaan"),
     PF_bend_angle_5: Yup.number()
-      .min(0, "ei voi olla negatiivinen")
-      .required("vaaditaan"),
+      .min(0, "PF_bend_angle_5 ei voi olla negatiivinen")
+      .required("PF_bend_angle_5 vaaditaan"),
   }),
   weightfactors: Yup.object().shape({
     WF_bend: Yup.number()
@@ -205,34 +205,34 @@ const parametersValidationSchema = Yup.object().shape({
   }),
   wind_wf: Yup.object({
     mild_wind_fast_vessel: Yup.number()
-      .min(0, "ei voi olla negatiivinen")
-      .required("vaaditaan"),
+      .min(0, "mild_wind_fast_vessel ei voi olla negatiivinen")
+      .required("mild_wind_fast_vessel vaaditaan"),
     mild_wind_moderate_vessel: Yup.number()
-      .min(0, "ei voi olla negatiivinen")
-      .required("vaaditaan"),
+      .min(0, "mild_wind_moderate_vessel ei voi olla negatiivinen")
+      .required("mild_wind_moderate_vessel vaaditaan"),
     mild_wind_slow_vessel: Yup.number()
-      .min(0, "ei voi olla negatiivinen")
-      .required("vaaditaan"),
+      .min(0, "mild_wind_slow_vessel ei voi olla negatiivinen")
+      .required("mild_wind_slow_vessel vaaditaan"),
 
     moderate_wind_fast_vessel: Yup.number()
-      .min(0, "ei voi olla negatiivinen")
-      .required("vaaditaan"),
+      .min(0, "moderate_wind_fast_vessel ei voi olla negatiivinen")
+      .required("moderate_wind_fast_vessel vaaditaan"),
     moderate_wind_moderate_vessel: Yup.number()
-      .min(0, "ei voi olla negatiivinen")
-      .required("vaaditaan"),
+      .min(0, "moderate_wind_moderate_vessel ei voi olla negatiivinen")
+      .required("moderate_wind_moderate_vessel vaaditaan"),
     moderate_wind_slow_vessel: Yup.number()
-      .min(0, "ei voi olla negatiivinen")
-      .required("vaaditaan"),
+      .min(0, "moderate_wind_slow_vessel ei voi olla negatiivinen")
+      .required("moderate_wind_slow_vessel vaaditaan"),
 
     strong_wind_fast_vessel: Yup.number()
-      .min(0, "ei voi olla negatiivinen")
-      .required("vaaditaan"),
+      .min(0, "strong_wind_fast_vessel ei voi olla negatiivinen")
+      .required("strong_wind_fast_vessel vaaditaan"),
     strong_wind_moderate_vessel: Yup.number()
-      .min(0, "ei voi olla negatiivinen")
-      .required("vaaditaan"),
+      .min(0, "strong_wind_moderate_vessel ei voi olla negatiivinen")
+      .required("strong_wind_moderate_vessel vaaditaan"),
     strong_wind_slow_vessel: Yup.number()
-      .min(0, "ei voi olla negatiivinen")
-      .required("vaaditaan"),
+      .min(0, "strong_wind_slow_vessel ei voi olla negatiivinen")
+      .required("strong_wind_slow_vessel vaaditaan"),
   }),
 });
 
