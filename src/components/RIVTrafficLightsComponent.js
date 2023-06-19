b832e90d7cab94be045a56b855b0ba1fbd83c8a3
Victor Lindh <victor.lindh@cgi.com>
2023-06-19 16:11:05 +0300

    add new state, draw button, and new outlook

--- src/components/RIVTrafficLightsComponent.js
+++ src/components/RIVTrafficLightsComponent.js
@@ -1,11 +1,14 @@
-import { CardContent, Typography, Grid, Box } from "@mui/material";
+import { CardContent, Typography, Grid, Box, Button } from "@mui/material";
 import RIVTrafficLightContext from "contexts/RIVTrafficLightContext";
+import { useState } from "react";
 import { useContext } from "react";
 
 export default function RIVTrafficLightsComponent() {
   const { RIVTrafficLight, setRIVTraffiLight } = useContext(
     RIVTrafficLightContext
   );
+  const [tempRIVTrafficLight, setTempRIVTrafficLight] =
+    useState(RIVTrafficLight);
 
   return (
     <CardContent>
@@ -16,106 +19,129 @@ export default function RIVTrafficLightsComponent() {
       >
         Riskiarvojen esitysvärien raja-arvot:
       </Typography>
-      <Grid container spacing={1}>
-        <Grid item xs={2.5}>
-          <Box
-            sx={{
-              bgcolor: "green",
-              color: "success.contrastText",
-              p: 1,
-            }}
-          >
-            Vihreä
+      <Grid container item xs={9}>
+        <Grid
+          container
+          item
+          xs
+          justifyContent="space-evenly"
+          sx={{
+            bgcolor: "green",
+            color: "success.contrastText",
+            p: 1,
+          }}
+        >
+          <Box>
+            <label>{"RIV <"}</label>
+            <input
+              type="float"
+              required
+              value={tempRIVTrafficLight.green}
+              onChange={(ev) =>
+                setTempRIVTrafficLight({
+                  ...tempRIVTrafficLight,
+                  green: Number(ev.target.value),
+                })
+              }
+              style={{
+                width: 100,
+              }}
+            />
           </Box>
         </Grid>
-        <Grid item xs={9.5}>
-          <input
-            type="float"
-            required
-            defaultValue={0}
-            style={{
-              width: 100,
-            }}
-          />
-          <label>{"≤ RIV <"}</label>
-          <input
-            type="float"
-            required
-            value={RIVTrafficLight.green}
-            onChange={(ev) =>
-              setRIVTraffiLight({
-                ...RIVTrafficLight,
-                green: Number(ev.target.value),
-              })
-            }
-            style={{
-              width: 100,
-            }}
-          />
+        <Grid
+          container
+          item
+          xs
+          justifyContent="space-evenly"
+          sx={{ bgcolor: "yellow", color: "black", p: 1 }}
+        >
+          <Box>
+            <input
+              type="float"
+              required
+              value={tempRIVTrafficLight.green}
+              onChange={(ev) =>
+                setTempRIVTrafficLight({
+                  ...tempRIVTrafficLight,
+                  green: Number(ev.target.value),
+                })
+              }
+              style={{
+                width: 100,
+              }}
+            />
+            <label>{"≤ RIV <"}</label>
+            <input
+              type="float"
+              required
+              value={tempRIVTrafficLight.yellow}
+              onChange={(ev) =>
+                setTempRIVTrafficLight({
+                  ...tempRIVTrafficLight,
+                  yellow: Number(ev.target.value),
+                })
+              }
+              style={{
+                width: 100,
+              }}
+            />
+          </Box>
         </Grid>
-        <Grid item xs={2.5}>
-          <Box sx={{ bgcolor: "yellow", color: "black", p: 1 }}>Keltainen</Box>
+        <Grid
+          container
+          item
+          xs
+          justifyContent="space-evenly"
+          sx={{ bgcolor: "red", color: "white", p: 1 }}
+        >
+          <Box>
+            <label>{"RIV ≥"}</label>
+            <input
+              type="float"
+              required
+              value={tempRIVTrafficLight.yellow}
+              onChange={(ev) =>
+                setTempRIVTrafficLight({
+                  ...tempRIVTrafficLight,
+                  yellow: Number(ev.target.value),
+                })
+              }
+              style={{
+                width: 100,
+              }}
+            />
+          </Box>
         </Grid>
-        <Grid item xs={9.5}>
-          <input
-            type="float"
-            required
-            value={RIVTrafficLight.green}
-            onChange={(ev) =>
-              setRIVTraffiLight({
-                ...RIVTrafficLight,
-                green: Number(ev.target.value),
-              })
-            }
-            style={{
-              width: 100,
-            }}
-          />
-          <label>{"≤ RIV <"}</label>
-          <input
-            type="float"
-            required
-            value={RIVTrafficLight.yellow}
-            onChange={(ev) =>
-              setRIVTraffiLight({
-                ...RIVTrafficLight,
-                yellow: Number(ev.target.value),
-              })
-            }
-            style={{
-              width: 100,
-            }}
-          />
-        </Grid>
-        <Grid item xs={2.5}>
-          <Box sx={{ bgcolor: "red", color: "white", p: 1 }}>Punainen</Box>
-        </Grid>
-
-        <Grid item xs={9.5}>
-          <label>{"RIV ≥ "}</label>
-          <input
-            type="float"
-            required
-            value={RIVTrafficLight.yellow}
-            onChange={(ev) =>
-              setRIVTraffiLight({
-                ...RIVTrafficLight,
-                yellow: Number(ev.target.value),
-              })
-            }
-            style={{
-              width: 100,
-            }}
-          />
-        </Grid>
-        <Grid item xs={2.5}>
-          <Box sx={{ bgcolor: "gray", color: "black", p: 1 }}>Harmaa</Box>
-        </Grid>
-        <Grid item xs={9.5}>
+      </Grid>
+      <Grid container item xs={9}>
+        <Grid
+          justifyContent="space-evenly"
+          container
+          item
+          xs={4}
+          sx={{ bgcolor: "gray", color: "black", p: 1 }}
+        >
           <label>
             Piste esitetään harmaana, jos siltä puuttuu leveys tai syvyys, mikä
             vaikuttaa lopulliseen riskiarvoon.
           </label>
+        </Grid>
+        <Grid
+          item
+          xs={4}
+          container
+          justifyContent="space-evenly"
+          alignItems="center"
+        >
+          <Button
+            variant="contained"
+            onClick={() => {
+              setRIVTraffiLight(tempRIVTrafficLight);
+            }}
+          >
+            Aseta / Piirrä
+          </Button>
         </Grid>
         {/* Tarkista onko syvyys tai leveys np.NaN jos on niin laita trafficlight vihreäksi*/}
       </Grid>
