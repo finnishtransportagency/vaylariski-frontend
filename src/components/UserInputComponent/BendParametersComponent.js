abee75a22e9730d07a488b066a66ae104e99e60e
Victor Lindh <victor.lindh@cgi.com>
2023-07-14 12:14:18 +0300

    moved some old code, put info into tooltip

--- src/components/UserInputComponent/BendParametersComponent.js
+++ src/components/UserInputComponent/BendParametersComponent.js
@@ -6,45 +6,23 @@ import {
   Tooltip,
   InputLabel,
   Switch,
+  IconButton,
 } from "@mui/material";
 import { Field } from "formik";
 import { useContext, useEffect, useState } from "react";
-import apiClient from "http-common";
-import NotificationContext from "contexts/NotificationContext";
-import Form from "react-bootstrap/Form";
-import SelectedBoatContext from "contexts/SelectedBoatContext";
 import userInputDefault from "constants/UserInputDefault";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
 
 export default function BendParametersComponent(props) {
   const { formik } = props;
-  const [length, setLength] = useState(formik.values.boat.length);
-  const [beam, setBeam] = useState(formik.values.boat.beam);
-  const [draft, setDraft] = useState(formik.values.boat.draft);
-
-  function setChosenBoatFormikValue(newBoat) {
-    if (newBoat) {
-      formik.setValues({
-        ...formik.values,
-        boat: {
-          ...formik.values.boat,
-          length: newBoat.PITUUS ? newBoat.PITUUS : "",
-          beam: newBoat.LEVEYS ? newBoat.LEVEYS : "",
-          draft: newBoat.SYVAYS ? newBoat.SYVAYS : "",
-        },
-      });
-    } else {
-      formik.setValues({
-        ...formik.values,
-        boat: {
-          ...formik.values.boat,
-          length: userInputDefault.boat.length,
-          beam: userInputDefault.boat.beam,
-          draft: userInputDefault.boat.draft,
-        },
-      });
-    }
-  }
-
+  const [open, setOpen] = useState(false);
+  const handleTooltipClose = () => {
+    setOpen(false);
+  };
+  const handleTooltipOpen = () => {
+    setOpen((prevValue) => !prevValue);
+  };
   return (
     <>
       <Grid item xs={12}>
@@ -65,23 +43,47 @@ export default function BendParametersComponent(props) {
           PF
           <span style={{ verticalAlign: "sub", fontSize: 12 }}>bend1</span> ja
           sen raja-arvot
+          <ClickAwayListener onClickAway={handleTooltipClose}>
+            <Tooltip
+              placement="right"
+              arrow
+              title={
+                <label style={{ fontSize: 14 }}>
+                  PF
+                  <span style={{ verticalAlign: "sub", fontSize: 12 }}>
+                    bend1
+                  </span>{" "}
+                  on painokerroin mutkan jyrkkyydelle suhteessa säteeseen,
+                  aluksen kääntösäteeseen ja alukseen pituuteen.
+                  <br />
+                  BSI = R
+                  <span style={{ verticalAlign: "sub", fontSize: 12 }}>
+                    b
+                  </span>{" "}
+                  / C
+                  <span style={{ verticalAlign: "sub", fontSize: 12 }}>tr</span>{" "}
+                  * L, kun R on säde, C
+                  <span style={{ verticalAlign: "sub", fontSize: 12 }}>tr</span>{" "}
+                  on kerroin, joka ilmaisee aluksen kääntösäteen ja L on aluksen
+                  pituus.{" "}
+                </label>
+              }
+              PopperProps={{
+                disablePortal: true,
+              }}
+              onClose={handleTooltipClose}
+              open={open}
+              disableFocusListener
+              disableHoverListener
+              disableTouchListener
+            >
+              <IconButton onClick={handleTooltipOpen}>
+                <InfoOutlinedIcon fontSize="small" />
+              </IconButton>
+            </Tooltip>
+          </ClickAwayListener>
         </Typography>
       </Grid>
-      <Grid item xs={12}>
-        <label htmlFor="PF_bend_parameters">
-          {" "}
-          PF
-          <span style={{ verticalAlign: "sub", fontSize: 12 }}>bend1</span> on
-          painokerroin mutkan jyrkkyydelle suhteessa säteeseen, aluksen
-          kääntösäteeseen ja alukseen pituuteen. BSI = R
-          <span style={{ verticalAlign: "sub", fontSize: 12 }}>b</span> / C
-          <span style={{ verticalAlign: "sub", fontSize: 12 }}>tr</span> * L,
-          kun R on säde, C
-          <span style={{ verticalAlign: "sub", fontSize: 12 }}>tr</span> on
-          kerroin, joka ilmaisee aluksen kääntösäteen ja L on aluksen pituus.{" "}
-        </label>
-      </Grid>
-
       <Grid item xs={5}>
         <label>
           {"PF"}
