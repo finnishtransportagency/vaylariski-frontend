2e3f4b08db9194d22802c34709a6c63a7c149f81
Victor Lindh <victor.lindh@cgi.com>
2023-07-19 19:01:59 +0300

    fourth mui and removed old

--- src/components/UserInputComponent/WayareaParameterComponent.js
+++ src/components/UserInputComponent/WayareaParameterComponent.js
@@ -5,6 +5,7 @@ import {
   InputLabel,
   Select,
   MenuItem,
+  TextField,
 } from "@mui/material";
 import { Field } from "formik";
 
@@ -82,70 +83,33 @@ export default function WayareaParameterComponent(props) {
           <MenuItem value={"rough_and_hard"}>Epätasainen ja kova</MenuItem>
         </Select>
       </Grid>
-      <Grid item xs={12} />
-      <Grid item xs={4}>
-        <label htmlFor="navline.calculation_params.type"> Tyyppi: </label>
-      </Grid>
-      <Grid item xs={8}>
-        <Field component="select" name="navline.calculation_params.type">
-          <option value="inner">Väylän sisäosa</option>
-          <option value="outer">Väylän ulko-osa</option>
-        </Field>
-      </Grid>
-      <Grid item xs={4}>
-        <label htmlFor="navline.calculation_params.number_of_lanes">
-          {" "}
-          Kaistat:{" "}
-        </label>
-      </Grid>
-      <Grid item xs={8}>
-        <Field
-          component="select"
-          name="navline.calculation_params.number_of_lanes"
-        >
-          <option value={2}>Kaksisuuntainen väylä</option>
-          <option value={1}>Yksisuuntainen väylä</option>
-        </Field>
-      </Grid>
-      <Grid item xs={4}>
-        <label htmlFor="navline.calculation_params.bottom_surface">
-          {" "}
-          Pohja:{" "}
-        </label>
-      </Grid>
-      <Grid item xs={8}>
-        <Field
-          component="select"
-          name="navline.calculation_params.bottom_surface"
-        >
-          <option value="smooth_and_soft">Sileä ja pehmeä</option>
-          <option value="rough_and_hard">Epätasainen ja kova</option>
-        </Field>
-      </Grid>
-      <Grid item xs={4}>
-        <label htmlFor="navline.calculation_params.other.visibility">
-          {" "}
-          Näkyvyys (m):{" "}
-        </label>
-      </Grid>
-      <Grid item xs={8}>
+      <Grid item xs={6}>
+        <InputLabel id="navline.calculation_params.other.visibility">
+          Näkyvyys (m)
+        </InputLabel>
         <Tooltip
           placement="right"
           arrow
           title={formik.errors?.navline?.calculation_params?.other?.visibility}
         >
           <span>
-            <Field
-              className={
-                formik.errors?.navline?.calculation_params?.other?.visibility &&
-                "has-error"
+            <TextField
+              id="navline.calculation_params.visibility"
+              error={
+                !!formik.errors?.navline?.calculation_params?.other?.visibility
               }
-              component="input"
-              name="navline.calculation_params.other.visibility"
+              inputProps={{
+                step: "0.1",
+              }}
+              size="small"
+              fullWidth
               type="number"
-              required
-              style={{
-                width: 100,
+              value={formik.values.navline.calculation_params.other.visibility}
+              onChange={(e) => {
+                formik.setFieldValue(
+                  "navline.calculation_params.other.visibility",
+                  e.target.value
+                );
               }}
             />
           </span>
