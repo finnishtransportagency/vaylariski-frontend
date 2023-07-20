84ec1ffaeef5c8a5a87a696c11e16dce36f76acf
Victor Lindh <victor.lindh@cgi.com>
2023-07-20 14:46:19 +0300

    using mui, and fixed + better labels

--- src/components/UserInputComponent/WayareaDepthWFComponent.js
+++ src/components/UserInputComponent/WayareaDepthWFComponent.js
@@ -3,13 +3,10 @@ import {
   Typography,
   Tooltip,
   InputLabel,
-  Select,
-  MenuItem,
   TextField,
   IconButton,
   ClickAwayListener,
 } from "@mui/material";
-import { Field } from "formik";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 import { useState } from "react";
 
@@ -25,216 +22,318 @@ export default function WayareaDepthWFComponent(props) {
   };
   return (
     <Grid container spacing={1} paddingBottom={2}>
-      <Grid item xs={12}>
-        <Typography
-          component="span"
-          style={{
-            fontSize: 16,
-            fontWeight: 550,
-            verticalAlign: "middle",
-          }}
-          color="textSecondary"
-          gutterBottom
-        >
-          Väylän syvyyden painokerroin
-          <ClickAwayListener onClickAway={handleTooltipClose}>
-            <Tooltip
-              placement="right"
-              arrow
-              title={
-                <label style={{ fontSize: 14 }}>
-                  Syvyyden painokerroin kerrotaan aluksen leveydellä alla
-                  näkyvien määritysten perusteella, joihin vaikuttaa väylän
-                  syvyys ja aluksen syväys. Laskennassa voi käyttää oletusarvoja
-                  tai määrittää uudet painokertoimet.
-                </label>
-              }
-              PopperProps={{
-                disablePortal: true,
-              }}
-              onClose={handleTooltipClose}
-              open={open}
-              disableFocusListener
-              disableHoverListener
-              disableTouchListener
-            >
-              <IconButton onClick={handleTooltipOpen}>
-                <InfoOutlinedIcon fontSize="small" />
-              </IconButton>
-            </Tooltip>
-          </ClickAwayListener>
-        </Typography>
+      <Grid item container spacing={0}>
+        <Grid item xs={12}>
+          <Typography
+            component="span"
+            style={{
+              fontSize: 16,
+              fontWeight: 550,
+              verticalAlign: "middle",
+            }}
+            color="textSecondary"
+            gutterBottom
+          >
+            Väylän syvyyden painokerroin
+            <ClickAwayListener onClickAway={handleTooltipClose}>
+              <Tooltip
+                placement="right"
+                arrow
+                title={
+                  <label style={{ fontSize: 14 }}>
+                    Syvyyden painokerroin kerrotaan aluksen leveydellä alla
+                    näkyvien määritysten perusteella, joihin vaikuttaa väylän
+                    syvyys ja aluksen syväys. Laskennassa voi käyttää
+                    oletusarvoja tai määrittää uudet painokertoimet.
+                  </label>
+                }
+                PopperProps={{
+                  disablePortal: true,
+                }}
+                onClose={handleTooltipClose}
+                open={open}
+                disableFocusListener
+                disableHoverListener
+                disableTouchListener
+              >
+                <IconButton onClick={handleTooltipOpen}>
+                  <InfoOutlinedIcon fontSize="small" />
+                </IconButton>
+              </Tooltip>
+            </ClickAwayListener>
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography component="span" color="textSecondary">
+            <em>
+              S<span style={{ verticalAlign: "sub", fontSize: 12 }}>y</span> =
+              syvyys
+            </em>
+            ,{" "}
+            <em>
+              S<span style={{ verticalAlign: "sub", fontSize: 12 }}>ä</span> =
+              syväys
+            </em>
+          </Typography>
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        <label> Väylän sisäosa: </label>
+      <Grid item container spacing={1}>
+        <Grid item xs={12}>
+          <Typography color="textSecondary">Väylän sisäosa</Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <InputLabel
+            style={{ fontSize: 14 }}
+            id={"channel_depth_wf.deep_inner_channel"}
+          >
+            <em>
+              S<span style={{ verticalAlign: "sub", fontSize: 12 }}>y</span>
+            </em>
+            {" ≥ 1.5 × "}
+            <em>
+              S<span style={{ verticalAlign: "sub", fontSize: 12 }}>ä</span>
+            </em>
+          </InputLabel>
+          <Tooltip
+            placement="right"
+            arrow
+            title={formik.errors?.channel_depth_wf?.deep_inner_channel}
+          >
+            <span>
+              <TextField
+                fullWidth
+                id="channel_depth_wf.deep_inner_channel"
+                error={!!formik.errors?.channel_depth_wf?.deep_inner_channel}
+                InputProps={{ sx: { height: 30 } }}
+                inputProps={{
+                  step: "0.01",
+                }}
+                type="number"
+                value={formik.values.channel_depth_wf.deep_inner_channel}
+                onChange={(e) => {
+                  formik.setFieldValue(
+                    "channel_depth_wf.deep_inner_channel",
+                    e.target.value
+                  );
+                }}
+              />
+            </span>
+          </Tooltip>
+        </Grid>
+        <Grid item xs={4}>
+          <InputLabel
+            style={{ fontSize: 14 }}
+            id={"channel_depth_wf.medium_deep_inner_channel"}
+          >
+            1.15 ×{" "}
+            <em>
+              S<span style={{ verticalAlign: "sub", fontSize: 12 }}>ä</span>
+            </em>{" "}
+            ≤{" "}
+            <em>
+              S<span style={{ verticalAlign: "sub", fontSize: 12 }}>y</span>
+            </em>{" "}
+            {"<"} 1.5 ×{" "}
+            <em>
+              S<span style={{ verticalAlign: "sub", fontSize: 12 }}>ä</span>
+            </em>
+          </InputLabel>
+          <Tooltip
+            placement="right"
+            arrow
+            title={formik.errors?.channel_depth_wf?.medium_deep_inner_channel}
+          >
+            <span>
+              <TextField
+                fullWidth
+                id="channel_depth_wf.medium_deep_inner_channel"
+                error={
+                  !!formik.errors?.channel_depth_wf?.medium_deep_inner_channel
+                }
+                InputProps={{ sx: { height: 30 } }}
+                inputProps={{
+                  step: "0.01",
+                }}
+                type="number"
+                value={formik.values.channel_depth_wf.medium_deep_inner_channel}
+                onChange={(e) => {
+                  formik.setFieldValue(
+                    "channel_depth_wf.medium_deep_inner_channel",
+                    e.target.value
+                  );
+                }}
+              />
+            </span>
+          </Tooltip>
+        </Grid>
+        <Grid item xs={4}>
+          <InputLabel
+            style={{ fontSize: 14 }}
+            id={"channel_depth_wf.shallow_inner_channel"}
+          >
+            <em>
+              S<span style={{ verticalAlign: "sub", fontSize: 12 }}>y</span>
+            </em>{" "}
+            {"<"} 1.15 ×{" "}
+            <em>
+              S<span style={{ verticalAlign: "sub", fontSize: 12 }}>ä</span>
+            </em>
+          </InputLabel>
+          <Tooltip
+            placement="right"
+            arrow
+            title={formik.errors?.channel_depth_wf?.shallow_inner_channel}
+          >
+            <span>
+              <TextField
+                fullWidth
+                id="channel_depth_wf.shallow_inner_channel"
+                error={!!formik.errors?.channel_depth_wf?.shallow_inner_channel}
+                InputProps={{ sx: { height: 30 } }}
+                inputProps={{
+                  step: "0.01",
+                }}
+                type="number"
+                value={formik.values.channel_depth_wf.shallow_inner_channel}
+                onChange={(e) => {
+                  formik.setFieldValue(
+                    "channel_depth_wf.shallow_inner_channel",
+                    e.target.value
+                  );
+                }}
+              />
+            </span>
+          </Tooltip>
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        <label> Väylän ulko-osa: </label>
-      </Grid>
-      <Grid item xs={6}>
-        <div>
-          <label style={{ fontSize: 14 }}>syvyys ≥ 1.5 * syväys</label>
-        </div>
-
-        <Tooltip
-          placement="right"
-          arrow
-          title={formik.errors?.channel_depth_wf?.deep_inner_channel}
-        >
-          <span>
-            <Field
-              component="input"
-              className={
-                formik.errors?.channel_depth_wf?.deep_inner_channel &&
-                "has-error"
-              }
-              name="channel_depth_wf.deep_inner_channel"
-              type="number"
-              step="0.01"
-              required
-              style={{
-                width: 130,
-              }}
-              placeholder="painokerroin"
-            />
-          </span>
-        </Tooltip>
-        <div>
-          <label style={{ fontSize: 14 }}>
-            1.15*syväys ≤ syvyys 1.5*syväys
-          </label>
-        </div>
-        <Tooltip
-          placement="right"
-          arrow
-          title={formik.errors?.channel_depth_wf?.medium_deep_inner_channel}
-        >
-          <span>
-            <Field
-              className={
-                formik.errors?.channel_depth_wf?.medium_deep_inner_channel &&
-                "has-error"
-              }
-              component="input"
-              name="channel_depth_wf.medium_deep_inner_channel"
-              type="number"
-              step="0.01"
-              required
-              style={{
-                width: 130,
-              }}
-              placeholder="painokerroin"
-            />
-          </span>
-        </Tooltip>
-        <div>
-          <label style={{ fontSize: 14 }}>{"syvyys < 1.15*syväys"}</label>
-        </div>
-        <Tooltip
-          placement="right"
-          arrow
-          title={formik.errors?.channel_depth_wf?.shallow_inner_channel}
-        >
-          <span>
-            <Field
-              className={
-                formik.errors?.channel_depth_wf?.shallow_inner_channel &&
-                "has-error"
-              }
-              component="input"
-              name="channel_depth_wf.shallow_inner_channel"
-              type="number"
-              step="0.01"
-              required
-              style={{
-                width: 130,
-              }}
-              placeholder="painokerroin"
-            />
-          </span>
-        </Tooltip>
-      </Grid>
-      <Grid item xs={6}>
-        <div>
-          <label style={{ fontSize: 14 }}>syvyys ≥ 1.5 * syväys</label>
-        </div>
-        <Tooltip
-          placement="right"
-          arrow
-          title={formik.errors?.channel_depth_wf?.deep_outer_channel}
-        >
-          <span>
-            <Field
-              className={
-                formik.errors?.channel_depth_wf?.deep_outer_channel &&
-                "has-error"
-              }
-              component="input"
-              name="channel_depth_wf.deep_outer_channel"
-              type="number"
-              step="0.01"
-              required
-              style={{
-                width: 130,
-              }}
-              placeholder="painokerroin"
-            />
-          </span>
-        </Tooltip>
-        <div>
-          <label style={{ fontSize: 14 }}>
-            1.25*syväys ≤ syvyys 1.5*syväys
-          </label>
-        </div>
-        <Tooltip
-          placement="right"
-          arrow
-          title={formik.errors?.channel_depth_wf?.medium_deep_outer_channel}
-        >
-          <span>
-            <Field
-              className={
-                formik.errors?.channel_depth_wf?.medium_deep_outer_channel &&
-                "has-error"
-              }
-              component="input"
-              name="channel_depth_wf.medium_deep_outer_channel"
-              type="number"
-              step="0.01"
-              required
-              style={{
-                width: 130,
-              }}
-              placeholder="painokerroin"
-            />
-          </span>
-        </Tooltip>
-        <div>
-          <label style={{ fontSize: 14 }}>{"syvyys < 1.25*syväys"}</label>
-        </div>
-        <Tooltip
-          placement="right"
-          arrow
-          title={formik.errors?.channel_depth_wf?.shallow_outer_channel}
-        >
-          <span>
-            <Field
-              className={
-                formik.errors?.channel_depth_wf?.shallow_outer_channel &&
-                "has-error"
-              }
-              component="input"
-              name="channel_depth_wf.shallow_outer_channel"
-              type="number"
-              step="0.01"
-              required
-              style={{
-                width: 130,
-              }}
-              placeholder="painokerroin"
-            />
-          </span>
-        </Tooltip>
+      <Grid item container spacing={1}>
+        <Grid item xs={12}>
+          <Typography color="textSecondary">Väylän sisäosa</Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <InputLabel
+            style={{ fontSize: 14 }}
+            id={"channel_depth_wf.deep_outer_channel"}
+          >
+            <em>
+              S<span style={{ verticalAlign: "sub", fontSize: 12 }}>y</span>
+            </em>
+            {" ≥ 1.5 × "}
+            <em>
+              S<span style={{ verticalAlign: "sub", fontSize: 12 }}>ä</span>
+            </em>
+          </InputLabel>
+          <Tooltip
+            placement="right"
+            arrow
+            title={formik.errors?.channel_depth_wf?.deep_outer_channel}
+          >
+            <span>
+              <TextField
+                fullWidth
+                id="channel_depth_wf.deep_outer_channel"
+                error={!!formik.errors?.channel_depth_wf?.deep_outer_channel}
+                InputProps={{ sx: { height: 30 } }}
+                inputProps={{
+                  step: "0.01",
+                }}
+                type="number"
+                value={formik.values.channel_depth_wf.deep_outer_channel}
+                onChange={(e) => {
+                  formik.setFieldValue(
+                    "channel_depth_wf.deep_outer_channel",
+                    e.target.value
+                  );
+                }}
+              />
+            </span>
+          </Tooltip>
+        </Grid>
+        <Grid item xs={4}>
+          <InputLabel
+            style={{ fontSize: 14 }}
+            id={"channel_depth_wf.medium_deep_outer_channel"}
+          >
+            1.25 ×{" "}
+            <em>
+              S<span style={{ verticalAlign: "sub", fontSize: 12 }}>ä</span>
+            </em>{" "}
+            ≤{" "}
+            <em>
+              S<span style={{ verticalAlign: "sub", fontSize: 12 }}>y</span>
+            </em>{" "}
+            {"<"} 1.5 ×{" "}
+            <em>
+              S<span style={{ verticalAlign: "sub", fontSize: 12 }}>ä</span>
+            </em>
+          </InputLabel>
+          <Tooltip
+            placement="right"
+            arrow
+            title={formik.errors?.channel_depth_wf?.medium_deep_outer_channel}
+          >
+            <span>
+              <TextField
+                fullWidth
+                id="channel_depth_wf.medium_deep_outer_channel"
+                error={
+                  !!formik.errors?.channel_depth_wf?.medium_deep_outer_channel
+                }
+                InputProps={{ sx: { height: 30 } }}
+                inputProps={{
+                  step: "0.01",
+                }}
+                type="number"
+                value={formik.values.channel_depth_wf.medium_deep_outer_channel}
+                onChange={(e) => {
+                  formik.setFieldValue(
+                    "channel_depth_wf.medium_deep_outer_channel",
+                    e.target.value
+                  );
+                }}
+              />
+            </span>
+          </Tooltip>
+        </Grid>
+        <Grid item xs={4}>
+          <InputLabel
+            style={{ fontSize: 14 }}
+            id={"channel_depth_wf.shallow_outer_channel"}
+          >
+            <em>
+              S<span style={{ verticalAlign: "sub", fontSize: 12 }}>y</span>
+            </em>{" "}
+            {"<"} 1.25 ×{" "}
+            <em>
+              S<span style={{ verticalAlign: "sub", fontSize: 12 }}>ä</span>
+            </em>
+          </InputLabel>
+          <Tooltip
+            placement="right"
+            arrow
+            title={formik.errors?.channel_depth_wf?.shallow_outer_channel}
+          >
+            <span>
+              <TextField
+                fullWidth
+                id="channel_depth_wf.shallow_outer_channel"
+                error={!!formik.errors?.channel_depth_wf?.shallow_outer_channel}
+                InputProps={{ sx: { height: 30 } }}
+                inputProps={{
+                  step: "0.01",
+                }}
+                type="number"
+                value={formik.values.channel_depth_wf.shallow_outer_channel}
+                onChange={(e) => {
+                  formik.setFieldValue(
+                    "channel_depth_wf.shallow_outer_channel",
+                    e.target.value
+                  );
+                }}
+              />
+            </span>
+          </Tooltip>
+        </Grid>
       </Grid>
     </Grid>
   );
