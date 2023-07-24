cf190c558d54ba3a54ebdf72b5cb3da1373b060c
Victor Lindh <victor.lindh@gmail.com>
2023-07-24 15:35:19 +0300

    Analpa 2913 vaylan reuna ja reunan painokerroin rekatorointi 2 (#28)
    * rewrote logic, using mui with better spacing and labels
    
    * new component using helpers componenet
    
    * removed old stuff
    
    * added functionality for new input
    
    * added new input to component
    
    * removed unused import
    
    * fixed tooltip text for new input
    
    * better labels for old
    
    ---------
    
    Co-authored-by: Victor Lindh <victor.lindh@cgi.com>

---
+++ src/components/UserInputComponent/ChannelEdgeAndBankClearanceComponent.js
@@ -0,0 +1,138 @@
+import {
+  Grid,
+  Typography,
+  Tooltip,
+  IconButton,
+  ClickAwayListener,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  FormLabel,
+  Switch,
+} from "@mui/material";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import { useState } from "react";
+
+import { table, simpleInput } from "utils/bankClearanceWFHelpers";
+
+export default function ChannelEdgeAndBankClearanceComponent(props) {
+  const { formik } = props;
+  const [open, setOpen] = useState(false);
+  const [showOld, setShowOld] = useState(false);
+
+  const handleTooltipClose = () => {
+    setOpen(false);
+  };
+  const handleTooltipOpen = () => {
+    setOpen((prevValue) => !prevValue);
+  };
+  const tooltipTitle = (
+    <label style={{ fontSize: 14 }}>
+      Riskiarvon laskentaan valitaan väylän reunan tyyppi. Alla on esitetty myös
+      reunan painokertoin joihin vaikuttaa reunan tyyppi sekä aluksen
+      nopeusluokka. Painokerroin ja aluksen leveys kerrotaan laskennassa.
+      Laskennassa voi käyttää oletusarvoja tai määrittää uudet painokertoimet.
+    </label>
+  );
+
+  return (
+    <Grid container spacing={1} paddingBottom={2}>
+      <Grid item xs={12}>
+        <Typography
+          component="span"
+          style={{
+            fontSize: 16,
+            fontWeight: 550,
+            verticalAlign: "middle",
+          }}
+          color="textSecondary"
+          gutterBottom
+        >
+          Väylän reuna ja reunan painokerroin
+          <ClickAwayListener onClickAway={handleTooltipClose}>
+            <Tooltip
+              arrow
+              placement="right"
+              title={tooltipTitle}
+              PopperProps={{
+                disablePortal: true,
+              }}
+              onClose={handleTooltipClose}
+              open={open}
+              disableFocusListener
+              disableHoverListener
+              disableTouchListener
+            >
+              <IconButton onClick={handleTooltipOpen}>
+                <InfoOutlinedIcon fontSize="small" />
+              </IconButton>
+            </Tooltip>
+          </ClickAwayListener>
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <FormLabel id={`navline.calculation_params.channel_edge-group-label`}>
+          Reunan tyyppi
+        </FormLabel>
+        <RadioGroup
+          row
+          aria-labelledby={`navline.calculation_params.channel_edge-group-label`}
+          name={"navline.calculation_params.channel_edge"}
+          sx={{
+            width: "100%",
+            justifyContent: "space-between",
+          }}
+          value={formik.values.navline.calculation_params.channel_edge}
+          defaultValue="gentle_slope"
+          onChange={(e) => {
+            formik.setFieldValue(
+              "navline.calculation_params.channel_edge",
+              e.target.value
+            );
+          }}
+        >
+          <FormControlLabel
+            value="gentle_slope"
+            control={<Radio />}
+            label={"Loiva kaltevuus"}
+          />
+          <FormControlLabel
+            value="sloping_edges"
+            control={<Radio />}
+            label={" Viistot reunat"}
+          />
+          <FormControlLabel
+            value="steep_and_hard"
+            control={<Radio />}
+            label={"Jyrkkä ja kova"}
+          />
+        </RadioGroup>
+      </Grid>
+      {simpleInput(formik)}
+      <Grid
+        item
+        xs={6}
+        justifyContent="center"
+        sx={{ display: "flex", alignItems: "center" }}
+      >
+        <Typography
+          style={{
+            fontSize: 14,
+            fontWeight: 275,
+          }}
+          color="textSecondary"
+          gutterBottom
+        >
+          Näytä vanha
+          <Switch
+            size="small"
+            checked={showOld}
+            onChange={(e) => setShowOld(e.target.checked)}
+            inputProps={{ "aria-label": "controlled" }}
+          />
+        </Typography>
+      </Grid>
+      {showOld ? table(formik) : null}
+    </Grid>
+  );
+}
---
+++ src/utils/bankClearanceWFHelpers.js
@@ -0,0 +1,150 @@
+import { TextField, Grid, Tooltip, InputLabel } from "@mui/material";
+
+const formikValueCategory = "bank_clearance_wf";
+const formikValuePrefix = "edge_category";
+const boatSpeedOptions = ["fast", "moderate", "slow"];
+const edgeOptions = ["gentle", "sloping", "steep"];
+
+const getFinnishBoatSpeedText = (boatSpeed) => {
+  let result = "";
+  switch (boatSpeed) {
+    case boatSpeedOptions[0]:
+      result = "Nopea";
+      break;
+    case boatSpeedOptions[1]:
+      result = "Keskiverto";
+      break;
+    case boatSpeedOptions[2]:
+      result = "Hidas";
+      break;
+  }
+  return result;
+};
+
+const getFinnishEdgeText = (edge) => {
+  let result = "";
+  switch (edge) {
+    case edgeOptions[0]:
+      result = "Loiva kaltevuus";
+      break;
+    case edgeOptions[1]:
+      result = "Viistot reunat";
+      break;
+    case edgeOptions[2]:
+      result = "Jyrkkä ja kova";
+      break;
+  }
+  return result;
+};
+
+const label = (id, boatSpeed, edge = "") => {
+  const labelText = `${getFinnishEdgeText(edge)}, ${getFinnishBoatSpeedText(
+    boatSpeed
+  )}`;
+
+  return (
+    <InputLabel style={{ fontSize: 14 }} id={`${formikValueCategory}.${id}`}>
+      {labelText}
+    </InputLabel>
+  );
+};
+
+const input = (id, formik, helperText = "") => {
+  return (
+    <>
+      <Tooltip
+        placement="right"
+        arrow
+        title={formik.errors?.[formikValueCategory]?.[id]}
+      >
+        <span>
+          <TextField
+            id={`${formikValueCategory}.${id}`}
+            error={!!formik.errors?.[formikValueCategory]?.[id]}
+            InputProps={{ sx: { height: 30 } }}
+            fullWidth
+            inputProps={{
+              step: "0.01",
+            }}
+            helperText={helperText}
+            type="number"
+            value={formik.values[formikValueCategory][id]}
+            onChange={(e) => {
+              formik.setFieldValue(
+                `${formikValueCategory}.${id}`,
+                e.target.value
+              );
+            }}
+          />
+        </span>
+      </Tooltip>
+    </>
+  );
+};
+
+const cell = (edge, boatSpeed, formik) => {
+  const id = `${formikValuePrefix}_${edge}_${boatSpeed}`;
+  return (
+    <Grid item xs={4} key={id}>
+      {label(id, boatSpeed, edge)}
+      {input(id, formik)}
+    </Grid>
+  );
+};
+const row = (edges, boatSpeed, formik) => {
+  return (
+    <Grid
+      container
+      item
+      alignItems="flex-end"
+      spacing={1}
+      key={`${formikValueCategory}.${boatSpeed}`}
+    >
+      {edges.map((edge) => {
+        return cell(edge, boatSpeed, formik);
+      })}
+    </Grid>
+  );
+};
+
+export const table = (formik) => {
+  return (
+    <Grid container item spacing={1} paddingBottom={2}>
+      {boatSpeedOptions.map((boatSpeed) => {
+        return row(edgeOptions, boatSpeed, formik);
+      })}
+    </Grid>
+  );
+};
+
+export const simpleInput = (formik) => {
+  const boatSpeed = formik.values.boat.speed;
+  const selectedChannelEdge =
+    formik.values.navline.calculation_params.channel_edge;
+  let edge = "";
+  switch (selectedChannelEdge) {
+    case "gentle_slope":
+      edge = edgeOptions[0];
+      break;
+    case "sloping_edges":
+      edge = edgeOptions[1];
+      break;
+    case "steep_and_hard":
+      edge = edgeOptions[2];
+      break;
+  }
+
+  const id = `${formikValuePrefix}_${edge}_${boatSpeed}`;
+  return (
+    <Grid item xs={6}>
+      <InputLabel style={{ fontSize: 14 }} id={`${formikValueCategory}.${id}`}>
+        Reunan painokerroin
+      </InputLabel>
+      {input(
+        id,
+        formik,
+        `Valittu aluksen nopeusluokka: ${getFinnishBoatSpeedText(boatSpeed)}`
+      )}
+    </Grid>
+  );
+};
