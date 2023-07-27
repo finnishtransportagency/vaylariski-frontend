257bf2bc8b85d890cd4631a90d523c5aeeea2be0
Victor Lindh <victor.lindh@cgi.com>
2023-07-27 20:19:29 +0300

    use CustomNumber in WeightFactorsComponent

--- src/components/UserInputComponent/WeightFactorsComponent.js
+++ src/components/UserInputComponent/WeightFactorsComponent.js
@@ -1,10 +1,5 @@
-import {
-  Grid,
-  Typography,
-  InputLabel,
-  TextField,
-  Tooltip,
-} from "@mui/material";
+import { Grid, Typography } from "@mui/material";
+import CustomNumber from "components/customInputs/CustomNumber";
 
 export default function WeightFactorsComponent(props) {
   const { formik } = props;
@@ -17,45 +12,16 @@ export default function WeightFactorsComponent(props) {
     ["WF_light_pollution", "Taustavalon voimakkuus"],
   ];
 
-  const input = (id) => {
-    return (
-      <Tooltip
-        placement="right"
-        arrow
-        title={formik.errors?.weightfactors?.[id]}
-      >
-        <span>
-          <TextField
-            fullWidth
-            id={`weightfactors.${id}`}
-            error={!!formik.errors?.weightfactors?.[id]}
-            InputProps={{ sx: { height: 30 } }}
-            type="number"
-            value={formik.values.weightfactors[id]}
-            onChange={(e) => {
-              formik.setFieldValue(
-                `weightfactors.${id}`,
-                Number(e.target.value)
-              );
-            }}
-          />
-        </span>
-      </Tooltip>
-    );
-  };
-  const label = (id, name) => {
-    return (
-      <InputLabel style={{ fontSize: 14 }} id={`weightfactors.${id}`}>
-        {name}
-      </InputLabel>
-    );
-  };
   const cell = (id, name) => {
     return (
-      <Grid item xs={6} key={`weightfactors.${id}`}>
-        {label(id, name)}
-        {input(id)}
-      </Grid>
+      <CustomNumber
+        key={`weightfactors.${id}`}
+        formik={formik}
+        formikName={`weightfactors.${id}`}
+        label={name}
+        xs={6}
+        step={1}
+      />
     );
   };
 
