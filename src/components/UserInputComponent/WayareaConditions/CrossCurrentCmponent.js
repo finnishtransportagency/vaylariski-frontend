8c223b878b5073bccb484695747feeca5a76300a
Victor Lindh <victor.lindh@cgi.com>
2023-07-27 16:08:26 +0300

    use CustomRadio in CrossCurrentComponent

--- src/components/UserInputComponent/WayareaConditions/CrossCurrentCmponent.js
+++ src/components/UserInputComponent/WayareaConditions/CrossCurrentCmponent.js
@@ -1,102 +1,39 @@
-import {
-  Grid,
-  Typography,
-  RadioGroup,
-  Radio,
-  FormControlLabel,
-} from "@mui/material";
-import { useState } from "react";
+import CustomRadio from "components/customInputs/CustomRadio";
 
 export default function CrossCurrentCmponent(props) {
   const { formik } = props;
-  const [selected, setSelected] = useState("moderate");
-
-  const buttonInfo = [
+  const RadioButtonPropsArr = [
     {
       value: "negligible",
       label: "Olematon",
-      range: "0 ≤ v < 0.2",
+      labelHelperText: "0 ≤ v < 0.2",
     },
     {
       value: "low",
       label: "Heikko",
-      range: "0.2 ≤ v < 0.5",
+      labelHelperText: "0.2 ≤ v < 0.5",
     },
     {
       value: "moderate",
       label: "Keskiverto",
-      range: "0.5 ≤ v < 1.5",
+      labelHelperText: "0.5 ≤ v < 1.5",
     },
     {
       value: "strong",
       label: "Voimakas",
-      range: "1.5 ≤ v < 2",
+      labelHelperText: "1.5 ≤ v < 2",
     },
   ];
 
-  const radioButtons = () => {
-    return buttonInfo.map((o) => {
-      return (
-        <FormControlLabel
-          key={`navline.calculation_params.operating_conditions.cross_current_speed.${o.value}`}
-          value={o.value}
-          control={<Radio />}
-          label={
-            <>
-              {o.label}
-              <br />
-              <Typography
-                style={{ fontSize: 14 }}
-                color="textSecondary"
-                gutterBottom
-              >
-                {o.range}
-              </Typography>
-            </>
-          }
-        />
-      );
-    });
-  };
-
   return (
-    <>
-      <Grid item xs={12}>
-        <Typography
-          style={{
-            fontSize: 16,
-            verticalAlign: "middle",
-          }}
-          color="textSecondary"
-          gutterBottom
-          component="span"
-        >
-          Poikkivirtaus (v [solmu])
-        </Typography>
-      </Grid>
-      <Grid item xs={12}>
-        <RadioGroup
-          row
-          name={
-            "navline.calculation_params.operating_conditions.cross_current_speed"
-          }
-          sx={{
-            width: "100%",
-            justifyContent: "space-between",
-          }}
-          value={selected}
-          defaultValue="negligible"
-          onChange={(e) => {
-            formik.setFieldValue(
-              "navline.calculation_params.operating_conditions.cross_current_speed",
-              e.target.value
-            );
-            setSelected(e.target.value);
-          }}
-        >
-          {radioButtons()}
-        </RadioGroup>
-      </Grid>
-    </>
+    <CustomRadio
+      formik={formik}
+      formikName={
+        "navline.calculation_params.operating_conditions.cross_current_speed"
+      }
+      defaultValue={RadioButtonPropsArr[0].value}
+      buttonPropsArr={RadioButtonPropsArr}
+      formLabelText="Poikkivirtaus (v [solmu])"
+    />
   );
 }
