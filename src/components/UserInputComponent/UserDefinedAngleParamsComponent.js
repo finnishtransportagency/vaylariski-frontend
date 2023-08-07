15e8813861442601b8762af42e7a5a64d9dce46f
Henriikka Harinen <henriikka.harinen@cgi.com>
2023-08-07 15:27:24 +0300

    changed layout and added autocomplete to gdogid

--- src/components/UserInputComponent/UserDefinedAngleParamsComponent.js
+++ src/components/UserInputComponent/UserDefinedAngleParamsComponent.js
@@ -3,14 +3,14 @@ import {
   Grid,
   InputLabel,
   TextField,
-  Tooltip,
   Typography,
+  Autocomplete,
 } from "@mui/material";
 import { Stack } from "@mui/system";
-import { Field, FieldArray } from "formik";
+import { FieldArray } from "formik";
 import PropTypes from "prop-types";
 import SelectedWayareaWithNoGDOGIDContext from "contexts/SelectedWayareaWithNoGDOGIDContext";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
@@ -20,7 +20,8 @@ import BendRadiusIcon from "icons/BendRadiusIcon";
 import BendRadiusIconSelected from "icons/BendRadiusIconSelected";
 import BendAngleIcon from "icons/BendAngleIcon";
 import BendAngleIconSelected from "icons/BendAngleIconSelected";
-// import BendAngleIconSelected from "icons/BendAngleIconSelected";
+import SelectedWayareaContext from "contexts/SelectedWayareaContext";
+import apiClient from "http-common";
 
 {
   /* Käyttäjän voi halutessaan ylikirjottaa kannassa lasketut SADE, BEND_ANGLE, S_BEND arvot antamilleen navigointilinjoille (GDO_GID) */
@@ -33,13 +34,74 @@ export default function UserDefinedAngleParamsComponent(props) {
     S_BEND: "",
     BEND_ANGLE: "",
   };
-  const { selectedWayareaWithNoGDOGID } = useContext(
-    SelectedWayareaWithNoGDOGIDContext
-  );
 
-  const [isSelected, setIsSelected] = useState(false);
-  const handleButtonClick = () => {
-    setIsSelected(!isSelected);
+  const { selectedWayarea } = useContext(SelectedWayareaContext);
+  const [selectedGDO_GIDs, setSelectedGDO_GIDs] = useState([]);
+
+  const [textFieldHasContent, setTextFieldHasContent] = useState([]);
+  const { selectedWayareaWithNoGDOGID, setSelectedWayareaWithNoGDOGID } =
+    useContext(SelectedWayareaWithNoGDOGIDContext);
+  const [allGDOGIDs, setAllGDOGIDs] = useState([]);
+
+  const handleMenuItemClick = (event, newValue, index) => {
+    formik.setFieldValue(`navline_angle_params.${index}.GDO_GID`, newValue);
+    setSelectedGDO_GIDs((prevSelectedGDO_GIDs) => {
+      const updatedSelectedGDO_GIDs = [...prevSelectedGDO_GIDs];
+      updatedSelectedGDO_GIDs[index] = newValue;
+      return updatedSelectedGDO_GIDs;
+    });
+  };
+  const handleRemoveRow = (index) => {
+    formik.setFieldValue(`navline_angle_params.${index}.GDO_GID`, null); // Reset the GDO_GID value
+    setSelectedGDO_GIDs((prevSelectedGDO_GIDs) => {
+      const updatedSelectedGDO_GIDs = [...prevSelectedGDO_GIDs];
+      updatedSelectedGDO_GIDs[index] = null;
+      return updatedSelectedGDO_GIDs;
+    });
+    formik.values.navline_angle_params.splice(index, 1); // Remove the row from formik values
+    formik.setFieldValue("navline_angle_params", [
+      ...formik.values.navline_angle_params,
+    ]); // Update formik values
+  };
+  useEffect(() => {
+    if (selectedWayarea) {
+      const path = "gdo_gids_for_vaylat";
+      apiClient
+        .get(path, {
+          params: {
+            VAYLAT: selectedWayarea.VAYLAT,
+          },
+        })
+        .then((response) => {
+          if (!response.data.GDO_GID.length) {
+            setSelectedWayareaWithNoGDOGID(true);
+          } else setSelectedWayareaWithNoGDOGID(false);
+          setAllGDOGIDs(response.data.GDO_GID);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [selectedWayarea]);
+
+  useEffect(() => {
+    // Initialize the state with an array of objects, each representing a TextField
+    setTextFieldHasContent(
+      formik.values.navline_angle_params.map((el) => ({
+        SADE: el.SADE !== "",
+        BEND_ANGLE: el.BEND_ANGLE !== "",
+        S_BEND: el.S_BEND !== "",
+      }))
+    );
+  }, [formik.values.navline_angle_params]);
+
+  // Function to handle text changes in the TextField and update the icon state
+  const handleTextFieldChange = (index, fieldName, value) => {
+    setTextFieldHasContent((prevTextFieldHasContent) => {
+      const updatedTextFieldHasContent = [...prevTextFieldHasContent];
+      updatedTextFieldHasContent[index][fieldName] = value !== "";
+      return updatedTextFieldHasContent;
+    });
   };
   return (
     <Grid container spacing={1} style={{ padding: 20 }}>
@@ -52,133 +114,143 @@ export default function UserDefinedAngleParamsComponent(props) {
           {...other}
         >
           <Typography
-            style={{ fontSize: 16, margin: 5 }}
+            style={{ fontSize: 16 }}
             color="textSecondary"
             gutterBottom
           >
-            Voit muokata laskentaa varten haluamasi navigointilinjan mutkan
-            sädettä, suuntakulmaa ja/tai S-mutkan pituutta. Tee valitulle
+            Voit muokata laskentaa varten navigointilinjojen mutkan sädettä,
+            suuntakulmaa ja/tai S-mutkan pituutta. Tee valitulle
             navigointilinjalle (GDO_GID) kaikki haluamasi muutokset samalle
-            riville. Kaikkia kenttiä ei ole kuitenkaan pakko täyttää. Voit
-            muokata useampaa navigointilinjaa, mutta tällä hetkellä ohjelma ei
-            hyväksy tyhjiä rivejä, joten ne tulee poistaa ennen lähettämistä.
+            riville.
           </Typography>
-
-          {/* <Button onClick={handleButtonClick}>
-            <Typography
-              style={{ fontSize: 14, margin: 5 }}
-              color="textSecondary"
-              gutterBottom
-            >
-              Säde
-            </Typography>
-            {isSelected ? <BendRadiusIconSelected /> : <BendRadiusIcon />}
-          </Button>
-          <Button onClick={handleButtonClick}>
-            <Typography
-              style={{ fontSize: 14, margin: 5 }}
-              color="textSecondary"
-              gutterBottom
-            >
-              Suuntakulma
-            </Typography>
-            {isSelected ? <BendAngleIconSelected /> : <BendAngleIcon />}
-          </Button>
-          <Button onClick={handleButtonClick}>
-            <Typography
-              style={{ fontSize: 14, margin: 5 }}
-              color="textSecondary"
-              gutterBottom
-            >
-              S-mutka
-            </Typography>
-            {isSelected ? <SBendIconSelected /> : <SBendIcon />}
-          </Button> */}
           {tabValue === tabIndex && (
             <FieldArray name="navline_angle_params">
               {({ remove, push }) => (
                 <div>
                   {formik.values.navline_angle_params.length > 0 &&
                     formik.values.navline_angle_params.map((el, index) => (
-                      <Stack direction="row" spacing={1} key={index}>
+                      <Stack
+                        direction="row"
+                        spacing={1}
+                        key={index}
+                        paddingBottom={1}
+                      >
                         <Grid>
-                          <InputLabel
-                            style={{ fontSize: 14 }}
-                            // id={"boat.length"}
-                          >
+                          <InputLabel style={{ fontSize: 14 }}>
                             GDO_GID
                           </InputLabel>
+                          <Autocomplete
+                            fullWidth
+                            id={`navline_angle_params.${index}.GDO_GID`}
+                            options={allGDOGIDs}
+                            getOptionLabel={(option) => option.toString() ?? ""}
+                            value={selectedGDO_GIDs[index] || null}
+                            onChange={(ev, newValue) =>
+                              handleMenuItemClick(ev, newValue, index)
+                            }
+                            size="small"
+                            renderInput={(params) => (
+                              <TextField
+                                required
+                                style={{
+                                  width: 220,
+                                }}
+                                {...params}
+                              />
+                            )}
+                          />
+                        </Grid>
+
+                        <Grid>
+                          <InputLabel style={{ fontSize: 14 }}>Säde</InputLabel>
+
                           <TextField
                             fullWidth
-                            id="navline_angle_params.${index}.GDO_GID"
-                            required
-                            type="float"
-                            InputProps={{ sx: { height: 30 } }}
-                            inputProps={{
-                              step: "0.1",
-                            }}
-                            value={formik.values.navline_angle_params.GDO_GID}
+                            InputProps={{ sx: { height: 40 } }}
+                            name={`navline_angle_params.${index}.SADE`}
+                            value={
+                              formik.values.navline_angle_params[index]?.SADE ||
+                              ""
+                            }
                             onChange={(e) => {
-                              formik.setFieldValue(
-                                "navline_angle_params.${index}.GDO_GID",
+                              formik.handleChange(e);
+                              handleTextFieldChange(
+                                index,
+                                "SADE",
                                 e.target.value
                               );
                             }}
-                          />
-                        </Grid>
-                        <Grid>
-                          <InputLabel
-                            style={{ fontSize: 14 }}
-                            // id={"boat.length"}
-                          >
-                            Säde
-                          </InputLabel>
-
-                          <TextField
-                            id={`navline_angle_params.${index}.SADE`}
                             type="float"
-                            InputProps={{ sx: { height: 30 } }}
-                            inputProps={{
-                              step: "0.1",
-                            }}
                           />
                         </Grid>
+                        {textFieldHasContent[index]?.SADE ? (
+                          <BendRadiusIconSelected />
+                        ) : (
+                          <BendRadiusIcon />
+                        )}
                         <Grid>
-                          <InputLabel
-                            style={{ fontSize: 14 }}
-                            // id={"boat.length"}
-                          >
+                          <InputLabel style={{ fontSize: 14 }}>
                             Suuntakulma
                           </InputLabel>
 
                           <TextField
-                            id={`navline_angle_params.${index}.BEND_ANGLE`}
-                            type="float"
-                            InputProps={{ sx: { height: 30 } }}
-                            inputProps={{
-                              step: "0.1",
+                            fullWidth
+                            InputProps={{ sx: { height: 40 } }}
+                            name={`navline_angle_params.${index}.BEND_ANGLE`}
+                            value={
+                              formik.values.navline_angle_params[index]
+                                ?.BEND_ANGLE || ""
+                            }
+                            onChange={(e) => {
+                              formik.handleChange(e);
+                              handleTextFieldChange(
+                                index,
+                                "BEND_ANGLE",
+                                e.target.value
+                              );
                             }}
+                            type="float"
                           />
                         </Grid>
+                        {textFieldHasContent[index]?.BEND_ANGLE ? (
+                          <BendAngleIconSelected />
+                        ) : (
+                          <BendAngleIcon />
+                        )}
                         <Grid>
-                          <InputLabel
-                            style={{ fontSize: 14 }}
-                            // id={"boat.length"}
-                          >
+                          <InputLabel style={{ fontSize: 14 }}>
                             S-mutkan pituus
                           </InputLabel>
 
                           <TextField
-                            id={`navline_angle_params.${index}.S_BEND`}
-                            type="float"
-                            InputProps={{ sx: { height: 30 } }}
-                            inputProps={{
-                              step: "0.1",
+                            fullWidth
+                            InputProps={{ sx: { height: 40 } }}
+                            name={`navline_angle_params.${index}.S_BEND`}
+                            value={
+                              formik.values.navline_angle_params[index]
+                                ?.S_BEND || ""
+                            }
+                            onChange={(e) => {
+                              formik.handleChange(e);
+                              handleTextFieldChange(
+                                index,
+                                "S_BEND",
+                                e.target.value
+                              );
                             }}
+                            type="float"
                           />
                         </Grid>
+                        {textFieldHasContent[index]?.S_BEND ? (
+                          <SBendIconSelected />
+                        ) : (
+                          <SBendIcon />
+                        )}
                         <Button
-                          onClick={() => remove(index)}
+                          onClick={() => {
+                            remove(index);
+                            handleRemoveRow(index);
+                          }}
                           style={{ marginTop: 12 }}
                         >
                           <RemoveCircleOutlineIcon style={{ margin: 3 }} />
