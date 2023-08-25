b7ea415ea023210d2c8fba01a55f64b026675915
Cedric Neergaard <75533906+CeduDev@users.noreply.github.com>
2023-08-25 16:31:40 +0300

    version bump (#44)

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ export const Header = () => (
           </p>
         </Col>
         <Col className="version-col">
-          <h5>Versio 1.1.0</h5>
+          <h5>Versio 1.1.1</h5>
         </Col>
       </Row>
     </Container>
