55f1c32d723a8d879adcb153bcb92e5ecdcf0ddf
henriikka.harinen <henriikka.harinen@cgi.com>
2024-02-15 15:53:53 +0200

    added point highlight and simplified if-conditions

--- src/views/MapView.js
+++ src/views/MapView.js
@@ -62,6 +62,15 @@ const geojsonMarkerOptionsGray = {
   fillOpacity: 0.8,
 };
 
+const highlighMapPoint = {
+  radius: 8,
+  // fillColor: "#83888a",
+  color: "black",
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0,
+};
+
 // When comparing routeline and navigationline the cross sign is used instead of circle
 const crossIconRed = L.divIcon({
   className: "material-icon",
@@ -74,6 +83,7 @@ const crossIconRed = L.divIcon({
   ),
   iconAnchor: [6.5, 9], // This is the spot where the circle markers are anchored by default (approx)
 });
+
 const crossIconYellow = L.divIcon({
   className: "material-icon",
   html: ReactDOMServer.renderToString(
@@ -85,6 +95,7 @@ const crossIconYellow = L.divIcon({
   ),
   iconAnchor: [6.5, 9], // This is the spot where the circle markers are anchored by default
 });
+
 const crossIconGreen = L.divIcon({
   className: "material-icon",
   html: ReactDOMServer.renderToString(
@@ -96,6 +107,7 @@ const crossIconGreen = L.divIcon({
   ),
   iconAnchor: [6.5, 9], // This is the spot where the circle markers are anchored by default
 });
+
 const crossIconGray = L.divIcon({
   className: "material-icon",
   html: ReactDOMServer.renderToString(
@@ -116,6 +128,7 @@ function GeoJSONMarkers() {
   const [geojsonFeatGroupWay, setGeojsonFeatGroupWay] = useState(
     new L.FeatureGroup()
   );
+  const [highlightFeatureGroup] = useState(new L.FeatureGroup());
   const { wayareaPolygons } = useContext(WayareaPolygonContext);
   const { selectedRowIndex, setSelectedRowIndex } =
     useContext(SelectedIndexContext);
@@ -131,18 +144,35 @@ function GeoJSONMarkers() {
   );
 
   function onEachFeature(feature, layer) {
-    // If feature have properties parse all of them and bind to layer
+    // If feature has properties, parse them and bind them to the layer
     if (feature.properties) {
       layer.on("click", () => {
         setMapPointClicked(true);
         setSelectedRowIndex(feature.properties.point_index);
+
+        // Clear the existing highlight feature group
+        highlightFeatureGroup.clearLayers();
+
+        // Add a new circle marker to the highlight feature group
+        const highlight = L.circleMarker(layer.getLatLng(), highlighMapPoint);
+        highlightFeatureGroup.addLayer(highlight);
+
+        // Add the highlight feature group to the map
+        highlightFeatureGroup.addTo(map);
+
+        // Original cirle markers to front so the bind popup works in re-clicking
+        if (map.hasLayer(geojsonFeatGroup)) {
+          geojsonFeatGroup.bringToFront();
+        }
       });
+
       layer.bindPopup(layerBindPopupString(feature));
     }
   }
 
   useEffect(() => {
     setGeojsonFeatGroupWay(geojsonFeatGroupWay.clearLayers());
+    setGeojsonFeatGroupWay(highlightFeatureGroup.clearLayers());
     const w_layers = new L.GeoJSON(wayareaPolygons, {
       pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, geojsonMarkerOptionsGray);
@@ -162,40 +192,39 @@ function GeoJSONMarkers() {
           selectedCalculationType == calculationTypeEnums.COMPARE &&
           feature.properties.VAYLAT == null // If calculation type is compare and VAYLAT is null crosses are used. VAYLAT null refers that point is routeline while only navigationlines have VAYLAT
         ) {
-          // return L.marker(latlng, { icon: crossIcon });
           if (
             feature.properties.W_channel == null ||
             feature.properties.W_channel_depth == null
           ) {
             return L.marker(latlng, { icon: crossIconGray });
-          } else {
-            if (feature.properties.RISK_INDEX_SUM < RIVTrafficLight.green) {
-              return L.marker(latlng, { icon: crossIconGreen });
-            } else if (
-              feature.properties.RISK_INDEX_SUM >= RIVTrafficLight.green &&
-              feature.properties.RISK_INDEX_SUM < RIVTrafficLight.yellow
-            ) {
-              return L.marker(latlng, { icon: crossIconYellow });
-            }
-            return L.marker(latlng, { icon: crossIconRed });
           }
+          if (feature.properties.RISK_INDEX_SUM < RIVTrafficLight.green) {
+            return L.marker(latlng, { icon: crossIconGreen });
+          }
+          if (
+            feature.properties.RISK_INDEX_SUM >= RIVTrafficLight.green &&
+            feature.properties.RISK_INDEX_SUM < RIVTrafficLight.yellow
+          ) {
+            return L.marker(latlng, { icon: crossIconYellow });
+          }
+          return L.marker(latlng, { icon: crossIconRed });
         } else {
           if (
             feature.properties.W_channel == null ||
             feature.properties.W_channel_depth == null
           ) {
             return L.circleMarker(latlng, geojsonMarkerOptionsGray);
-          } else {
-            if (feature.properties.RISK_INDEX_SUM < RIVTrafficLight.green) {
-              return L.circleMarker(latlng, geojsonMarkerOptionsGreen);
-            } else if (
-              feature.properties.RISK_INDEX_SUM >= RIVTrafficLight.green &&
-              feature.properties.RISK_INDEX_SUM < RIVTrafficLight.yellow
-            ) {
-              return L.circleMarker(latlng, geojsonMarkerOptionsYellow);
-            }
-            return L.circleMarker(latlng, geojsonMarkerOptionsRed);
           }
+          if (feature.properties.RISK_INDEX_SUM < RIVTrafficLight.green) {
+            return L.circleMarker(latlng, geojsonMarkerOptionsGreen);
+          }
+          if (
+            feature.properties.RISK_INDEX_SUM >= RIVTrafficLight.green &&
+            feature.properties.RISK_INDEX_SUM < RIVTrafficLight.yellow
+          ) {
+            return L.circleMarker(latlng, geojsonMarkerOptionsYellow);
+          }
+          return L.circleMarker(latlng, geojsonMarkerOptionsRed);
         }
       },
     });
