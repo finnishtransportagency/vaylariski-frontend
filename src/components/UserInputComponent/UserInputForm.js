85be85681aefa542f66fe12eb9d01bf5374f5229
Juha Keponen <juha.keponen@cgi.com>
2024-05-08 13:10:09 +0300

    fix component import

--- src/components/UserInputComponent/UserInputForm.js
+++ src/components/UserInputComponent/UserInputForm.js
@@ -21,6 +21,7 @@ import SelectCalculationType from "./SelectCalculationType";
 import SelectedCalculationTypeContext from "contexts/SelectedCalculationTypeContext";
 import { isSubmitDisabled } from "utils/ValidateSubmit";
 import { ValidateSubmitTooltip } from "./ValidateSubmitTooltip";
+import SorUBendComponent from './SorUBendComponent';
 
 function a11yProps(index) {
   return {
