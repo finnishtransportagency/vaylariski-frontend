e5269bffa2ec7a62309ec22a5589c2fc18ddbb5c
Vili Forsell <vili.forsell@elenium.fi>
2025-10-17 11:03:05 +0300

    Version 4.0.1.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ export const Header = () => (
           </p>
         </Col>
         <Col className="version-col">
-          <h5>Versio 4.0.0</h5>
+          <h5>Versio 4.0.1</h5>
         </Col>
       </Row>
     </Container>
