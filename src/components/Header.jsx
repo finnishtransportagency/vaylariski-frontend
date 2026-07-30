1ce3c1142e0d14d335adb63ed1b23b68c366e44c
Vili Forsell <vili.forsell@elenium.fi>
2026-07-30 06:49:50 +0300

    Vulnerability fix 30.07.2026

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ export const Header = () => (
           </p>
         </Col>
         <Col className="version-col">
-          <h5>Versio 4.1.2</h5>
+          <h5>Versio 4.1.3</h5>
         </Col>
       </Row>
     </Container>
